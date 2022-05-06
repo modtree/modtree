ffe14c31e9d0bd9994b752fa158290dc939acebf
nguyenvukhang <brew4k@gmail.com>
2022-05-06 09:50:46 +0800

    get mod

--- apps/database/functions/src/index.ts
+++ apps/database/functions/src/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import axios from "axios";
 import { firestore } from "firebase-admin";
 import { initializeApp } from 'firebase-admin/app'
+import { DocumentData } from '@google-cloud/firestore'
 
 initializeApp()
 
@@ -18,8 +19,33 @@ export const pullMod = functions.https.onRequest(async (req, res) => {
   const data = result.data;
   const collectionRef = firestore().collection('modules')
   await collectionRef.add(data)
-
   res.json({
     result: data,
+  });
+});
+
+export const getMod = functions.https.onRequest(async (req, res) => {
+  const search: Record<string, string> = req.body
+
+  const collectionRef = firestore().collection('modules')
+  // const q = firestore().collection('modules').where('acadYear', "==", '2021/2022').where().where()
+
+  const arr = Object.entries(search)
+  let query = collectionRef.where(arr[0][0], '==', arr[0][1])
+
+  for (let i = 1; i < arr.length; i++) {
+    query = query.where(arr[i][0] , "==", arr[i][1])
+  }
+
+  const snapshot = await query.get()
+
+  const result: DocumentData[] = []
+  snapshot.forEach(doc => {
+    const data = doc.data()
+    result.push(data)
+  })
+
+  res.json({
+    result,
   });
 });
