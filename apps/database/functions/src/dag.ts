c2e7e9f11708d525eac648278b1c38b3131c4fab
weiseng18 <tws.hci@gmail.com>
2022-05-06 14:21:32 +0800

    else branch

--- apps/database/functions/src/dag.ts
+++ apps/database/functions/src/dag.ts
@@ -49,6 +49,18 @@ class User {
 // }
 //
 
+function checkTree(prereqTree: PrereqTree, modulesCleared: string[], hardRequirements: string[]): boolean {
+  if (typeof prereqTree === 'string')
+    return hardRequirements.includes(prereqTree);
+  else if (Array.isArray(prereqTree.and)) {
+    return prereqTree.and.every((one: PrereqTree) => checkTree(one, modulesCleared, hardRequirements))
+  } else if (Array.isArray(prereqTree.or)) {
+    return prereqTree.or.some((one: PrereqTree) => checkTree(one, modulesCleared, hardRequirements))
+  } else {
+    return false;
+  }
+}
+
 export const test = https.onRequest(async (req, res) => {
   const user = new User();
   const degree = new Degree("Computer Science", ["CS1010S", "CS1231S"]);
@@ -59,6 +71,8 @@ export const test = https.onRequest(async (req, res) => {
     // get module into from database
     // if no prereqTree, return true
     // else traverse the tree
+
+    return checkTree(prereqTree, user.done, hardRequirements);
   })
   console.log(user, hardRequirements)
   res.json({
