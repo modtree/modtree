37f03a71b1b1cb980d1c4f95e7ddf150ade9293b
nguyenvukhang <brew4k@gmail.com>
2022-05-06 09:39:49 +0800

    can pull

--- apps/database/functions/src/index.ts
+++ apps/database/functions/src/index.ts
@@ -1,15 +1,25 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
+import { firestore } from "firebase-admin";
+import { initializeApp } from 'firebase-admin/app'
+
+initializeApp()
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((req, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+const nusmodsApi = (req: string) =>
+  `https://api.nusmods.com/v2/2021-2022/${req}.json`;
 
-export const pullMod = functions.https.onRequest((req, res) => {
-  const moduleCode = req.body.moduleCode
-  console.log(moduleCode)
-  res.json({moduleCode})
+export const pullMod = functions.https.onRequest(async (req, res) => {
+  const moduleCode = req.body.moduleCode;
+  const apiRequest = nusmodsApi(`modules/${moduleCode}`);
+  const result = await axios.get(apiRequest);
+  const data = result.data;
+  const collectionRef = firestore().collection('modules')
+  await collectionRef.add(data)
+
+  res.json({
+    result: data,
+  });
 });
--- apps/tests/src/pullMod.ts
+++ apps/tests/src/pullMod.ts
@@ -1,15 +1,26 @@
-import axios from 'axios'
+import axios from "axios";
 
-const base = "http://localhost:5001/mod-tree/us-central1"
+const base = "http://localhost:5001/mod-tree/us-central1";
 
 export function cloud(string: string): string {
-  return `${base}/${string}`
+  return `${base}/${string}`;
 }
 
+const fn = "pullMod";
 
-const fn = 'pullMod'
+const modList = [
+  "MA2101",
+  "MA1301",
+  "CS2109S",
+  "ES2660",
+  "CS3241",
+  "CS2309",
+  "CS3233",
+];
 
-axios.post(cloud(fn), { moduleCode: 'CS1010S' }).then((res) => {
-  const data = res.data
-  console.debug(data)
-})
+modList.forEach((mod) => {
+  axios.post(cloud(fn), { moduleCode: mod }).then((res) => {
+    const data = res.data;
+    console.debug(data);
+  });
+});
