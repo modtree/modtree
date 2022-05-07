1bb23060faf4eb86b0048f039d88ad8f56b27bea
nguyenvukhang <brew4k@gmail.com>
2022-05-07 23:35:25 +0800

    remove tsconfig.dev.json

--- apps/database/functions/.eslintrc.js
+++ apps/database/functions/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    project: ['tsconfig.json'],
     sourceType: 'module',
   },
   ignorePatterns: [
