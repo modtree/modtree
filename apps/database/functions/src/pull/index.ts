986d9af70568f31d7a42698a53f7d5b44b694b79
nguyenvukhang <brew4k@gmail.com>
2022-05-08 00:38:23 +0800

    shortened pull

--- apps/database/functions/src/pull/index.ts
+++ apps/database/functions/src/pull/index.ts
@@ -1,38 +1,24 @@
-// import { User, Degree } from "../classes";
 import { https } from 'firebase-functions'
 import { db } from '../firebase'
 import { DocumentReference, DocumentData } from '@google-cloud/firestore'
 import { ModuleCondensed } from '../../types/nusmods'
 import { getAllExistingValues } from '../classes'
 import axios from 'axios'
-
-const nusmodsApi = (req: string) =>
-  `https://api.nusmods.com/v2/2021-2022/${req}.json`
+import { nusmodsApi } from '../utils'
 
 export const pullModuleList = https.onRequest(async (req, res) => {
   const moduleList = db.collection('moduleList')
-
   const already = await getAllExistingValues('moduleList', 'moduleCode')
-
-  /* get existing modules */
-
-  const apiRequest = nusmodsApi('moduleList')
-  const result = await axios.get(apiRequest)
+  const result = await axios.get(nusmodsApi('moduleList'))
   const data: ModuleCondensed[] = result.data
-
-  const filtered = data.filter((x) => !already.exists(x.moduleCode))
-
   const stack: Promise<DocumentReference<DocumentData>>[] = []
-
-  filtered.forEach((x: ModuleCondensed) => {
-    stack.push(moduleList.add(x))
-  })
-
-  console.log(filtered)
-  console.log('added')
-
+  data
+    .filter((x) => !already.exists(x.moduleCode))
+    .forEach((x: ModuleCondensed) => {
+      stack.push(moduleList.add(x))
+    })
   await Promise.allSettled(stack)
   res.json({
-    result: data,
+    message: `Added ${stack.length} modules to database.`,
   })
 })
