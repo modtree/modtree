d14a13aada065bf9681324a5b66d64422af4a60a
weiseng18 <tws.hci@gmail.com>
2022-05-08 15:46:53 +0800

    implement addUser firebase function

---
+++ apps/backend/functions/src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  age: number
+
+  // @Column()
+  // comment: string
+}
