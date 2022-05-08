630f18ea8856e6b3aca4d8190ec7308896aa1a11
weiseng18 <tws.hci@gmail.com>
2022-05-08 15:54:11 +0800

    prettier

--- apps/backend/functions/src/index.ts
+++ apps/backend/functions/src/index.ts
@@ -6,28 +6,28 @@ import { User } from './entities/User'
 
 export const addUser = https.onRequest(async (req, res) => {
   // info
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const age = req.body.age;
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const age = req.body.age
 
   // create user
   const user = new User()
-  user.firstName = firstName;
-  user.lastName = lastName;
-  user.age = age;
+  user.firstName = firstName
+  user.lastName = lastName
+  user.age = age
 
   // save user to DB
   await AppDataSource.initialize().then(async () => {
-    await AppDataSource.manager.save(user);
+    await AppDataSource.manager.save(user)
     console.log('Saved a new user with id: ' + user.id)
 
     // list all users in DB
     console.log('Loading users from the database...')
     const users = await AppDataSource.manager.find(User)
     console.log('Loaded users: ', users)
-  });
+  })
 
   res.json({
-    message: "Successful addUser"
-  });
-});
+    message: 'Successful addUser'
+  })
+})
