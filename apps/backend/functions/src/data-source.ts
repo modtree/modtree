526b2f89dc921a6b8b6dee6f4389d11bef7a30d5
nguyenvukhang <brew4k@gmail.com>
2022-05-09 11:06:09 +0800

    can read from local source

--- apps/backend/functions/src/data-source.ts
+++ apps/backend/functions/src/data-source.ts
@@ -7,19 +7,13 @@ export const LocalSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  // username: process.env.MYSQL_USERNAME,
-  // password: process.env.MYSQL_PASSWORD,
+  username: process.env.MYSQL_USERNAME,
   database: 'modtree_1',
   synchronize: true,
   logging: false,
   entities: [User],
   migrations: [],
   subscribers: [],
-  ssl: {
-    ca: process.env.MYSQL_SERVER_CA,
-    cert: process.env.MYSQL_CLIENT_CERT,
-    key: process.env.MYSQL_CLIENT_KEY,
-  },
 })
 
 export const AppDataSource = new DataSource({
