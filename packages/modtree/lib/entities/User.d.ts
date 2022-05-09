35ca2760f1a808ef236613f5501a26df25973cde
nguyenvukhang <brew4k@gmail.com>
2022-05-09 11:43:29 +0800

    ignore lib files

--- packages/modtree/lib/entities/User.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
-}
--- packages/modtree/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { User } from './entities/User';
-export { User };
