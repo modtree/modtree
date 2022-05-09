13e716f6595a7b269bc064ea8f5800aa8eb11578
nguyenvukhang <brew4k@gmail.com>
2022-05-09 17:43:15 +0800

    remove and ignore lib

--- packages/ui/lib/Button.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare const Button: () => JSX.Element;
-//# sourceMappingURL=Button.d.ts.map
--- packages/ui/lib/Button.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export const Button = () => {
-    return _jsx("button", { children: "Boop" });
-};
