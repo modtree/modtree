9aaf86e8672bd33d6bd3a3c26ec4a48c0d06cf9c
weiseng18 <tws.hci@gmail.com>
2022-05-12 16:00:36 +0800

    remove comment

--- packages/database/src/entity/Degree.ts
+++ packages/database/src/entity/Degree.ts
@@ -1,5 +1,4 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm'
-//import { Module } from '../../types/nusmods'
 import { Module } from './Module'
 
 @Entity({ name: 'degree' })
