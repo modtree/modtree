cd3e4d3472de12e60cbc8dffa06fd9847a279161
weiseng18 <tws.hci@gmail.com>
2022-05-12 16:04:30 +0800

    disable new-cap check for functions

---
+++ packages/database/src/functions/.eslintrc.js
@@ -0,0 +1,13 @@
+const config = require('modtree-config/eslint-backend')
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  ...config,
+  rules: {
+    ...config.rules,
+    'new-cap': 'off',
+  }
+}
