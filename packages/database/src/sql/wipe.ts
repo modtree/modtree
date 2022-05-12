cf1334a1ddad41d42fead1f7181d2914c6202e44
nguyenvukhang <brew4k@gmail.com>
2022-05-12 20:25:57 +0800

    sql: wrote a wipe script

--- packages/database/src/sql/wipe.ts
+++ packages/database/src/sql/wipe.ts
@@ -1,4 +1,3 @@
-import { config } from '../config'
 import { createConnection } from 'mysql2/promise'
 import { log } from '../cli'
 import { restore } from './restore'
@@ -9,26 +8,25 @@ export namespace wipe {
    * and then recreates it for a completely fresh start
    * so ensure .env.test has the corrent database name.
    */
-  export async function database() {
-    const init = await createConnection(initConfig)
-    // drop the database if it exists
-    await init
-      .query(`DROP DATABASE ${config.database};`)
-      .then()
-      .catch(() => {
-        console.log('calm down and carry on')
-      })
-    // create the database again
-    await init
-      .query(`CREATE DATABASE ${config.database};`)
-      .then(() => {
-        log.yellow('Setup: created test database')
-      })
-      .catch(() => {
-        log.gray('Setup: test db already exists')
-      })
-    // restore the database state from /.sql/test-source.sql
-    await restore.file('test-source.sql')
-    await init.end()
+  export async function database(database: string, sqlFile: string) {
+    await createConnection(initConfig).then(async (connection) => {
+      // drop the database if it exists
+      await connection
+        .query(`DROP DATABASE ${database};`)
+        .then()
+        .catch(() => {
+          log.gray(`database [${database}] already non-existent`)
+        })
+      // create the database again
+      await connection
+        .query(`CREATE DATABASE ${database};`)
+        .then()
+        .catch(() => {
+          log.gray(`database [${database}] already exists`)
+        })
+      // restore the database state from /.sql/${sqlFile}
+      await restore.file(sqlFile)
+      await connection.end()
+    })
   }
 }
--- packages/database/tests/setup.ts
+++ packages/database/tests/setup.ts
@@ -1,32 +1,8 @@
-import { initConfig } from '../src/sql/config'
 import { config } from '../src/config'
-import { createConnection } from 'mysql2/promise'
-import { log } from '../src/cli'
-import { restore } from '../src/sql'
+import { wipe } from '../src/sql'
 
-/* a very aggressive function that drops the database
- * and then recreates it for a completely fresh start
- * so ensure .env.test has the corrent database name.
- */
-export default async function setup() {
-  const init = await createConnection(initConfig)
-  // drop the database if it exists
-  await init
-    .query(`DROP DATABASE ${config.database};`)
-    .then()
-    .catch(() => {
-      console.log('calm down and carry on')
-    })
-  // create the database again
-  await init
-    .query(`CREATE DATABASE ${config.database};`)
-    .then(() => {
-      log.yellow('Setup: created test database')
-    })
-    .catch(() => {
-      log.gray('Setup: test db already exists')
-    })
-  // restore the database state from /.sql/test-source.sql
-  await restore.file('test-source.sql')
-  await init.end()
+const setup = async () => {
+  await wipe.database(config.database, '.test.sql')
 }
+
+export default setup
