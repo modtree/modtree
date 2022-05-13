7748877d2ad41229a278b016a8f4901ca820db17
weiseng18 <tws.hci@gmail.com>
2022-05-13 16:03:48 +0800

    create checkTree util and User.canTakeModule

---
+++ packages/database/src/utils.ts
@@ -0,0 +1,30 @@
+import { PrereqTree } from '../types/nusmods'
+
+export namespace utils {
+  /**
+   * sees if a user is eligible to take a mod, given the modules he/she has cleared
+   * @param {PrereqTree} prereqTree
+   * @param {string[]} modulesCleared
+   * @return {boolean}
+   */
+  export function checkTree(
+    prereqTree: PrereqTree,
+    modulesCleared: string[]
+  ): boolean {
+    if (prereqTree === '') return true
+    else if (typeof prereqTree === 'string') {
+      return modulesCleared.includes(prereqTree)
+    } else if (Array.isArray(prereqTree.and)) {
+      return prereqTree.and.every((one: PrereqTree) =>
+        checkTree(one, modulesCleared)
+      )
+    } else if (Array.isArray(prereqTree.or)) {
+      return prereqTree.or.some((one: PrereqTree) =>
+        checkTree(one, modulesCleared)
+      )
+    } else {
+      console.warn('not supposed to be here')
+      return true
+    }
+  }
+}
