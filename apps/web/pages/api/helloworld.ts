11b586aa61420691626ee33fa4eaf07b7805496a
nguyenvukhang <brew4k@gmail.com>
2022-05-16 16:50:49 +0800

    remove unused file

--- apps/web/pages/api/helloworld.ts
+++
@@ -1,8 +0,0 @@
-import type {
-  NextApiRequest as Request,
-  NextApiResponse as Response,
-} from 'next'
-
-export default function handler(req: Request, res: Response) {
-  res.status(200).json({ name: 'John Doe' })
-}
