a292598ed34d1ae3f387da01a54464febfefd56f
nguyenvukhang <brew4k@gmail.com>
2022-05-16 10:44:46 +0800

    captialize search term

--- apps/web/components/Html.tsx
+++ apps/web/components/Html.tsx
@@ -2,6 +2,6 @@ import { ReactChild } from "react"
 
 export const H1 = (props: { children:ReactChild }) => {
   return (
-    <h1 className='text-4xl'>{props.children}</h1>
+    <h1 className='text-4xl mt-8 mb-4'>{props.children}</h1>
   )
 }
--- apps/web/components/Search.tsx
+++ apps/web/components/Search.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import { UseState } from '../types'
 
 const Search = (props: { queryState: UseState<string> }) => {
-  const [query, setQuery] = props.queryState
+  const setQuery = props.queryState[1]
+  const [display, setDisplay] = useState('')
+  function handleQuery(value: string) {
+    const upper = value.toUpperCase()
+    setDisplay(value)
+    setQuery(upper)
+  }
   return (
     <div>
       <div className="text-gray-600">Search for a module:</div>
-      <input className='w-full' value={query} onChange={(e) => setQuery(e.target.value)} />
+      <input className='w-full' value={display} onChange={(e) => handleQuery(e.target.value)} />
     </div>
   )
 }
