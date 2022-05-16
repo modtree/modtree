59e0eeef024d443112c7272c3cd52e0584a102ec
nguyenvukhang <brew4k@gmail.com>
2022-05-16 11:29:10 +0800

    connect to database

--- apps/web/pages/api/module/get/[moduleCode].ts
+++ apps/web/pages/api/module/get/[moduleCode].ts
@@ -2,11 +2,20 @@ import type {
   NextApiRequest as Request,
   NextApiResponse as Response,
 } from 'next'
+import { AppDataSource } from 'database'
 
-export default function handler(req: Request, res: Response) {
+export default async function handler(req: Request, res: Response) {
+  AppDataSource.initialize().then(() => {
+
+  }).catch((err) => {
+    console.log(err)
+    console.log('TypeORM failed to initialize connection to database')
+  }).finally(() => {
+    AppDataSource.destroy()
+  })
   const { moduleCode } = req.query
-  if (moduleCode ==='CS1010S') {
-    res.status(200).json({ requested: moduleCode, cache: "hit!" })
+  if (moduleCode === 'CS1010S') {
+    res.status(200).json({ requested: moduleCode, cache: 'hit!' })
     return
   }
   res.status(200).json({ requested: moduleCode })
