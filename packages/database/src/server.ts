607669b17c550c286a47b4bc1611e33ee2d615a3
nguyenvukhang <brew4k@gmail.com>
2022-05-16 16:55:06 +0800

    cleanup server.ts

--- packages/database/src/server.ts
+++ packages/database/src/server.ts
@@ -3,11 +3,10 @@ import * as bodyParser from 'body-parser'
 import { Request, Response } from 'express'
 import { AppDataSource } from './data-source'
 import { Routes } from './routes'
-import { ModuleCondensed } from './entity-repo/ModuleCondensed'
 import cors, { CorsOptions } from 'cors'
 
 const corsOpts: CorsOptions = {
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000',
 }
 
 AppDataSource.initialize()
@@ -39,32 +38,6 @@ AppDataSource.initialize()
         }
       )
     })
-
-    // setup express app here
-    // ...
-
-    // start express server
     app.listen(8080)
-
-    // // insert new users for test
-    // await AppDataSource.manager.save(
-    //   AppDataSource.manager.create(ModuleCondensed, {
-    //     moduleCode: 'MA2001',
-    //     moduleLevel: 2,
-    //     title: 'Linear Algebra 1',
-    //   })
-    // )
-    //
-    // await AppDataSource.manager.save(
-    //   AppDataSource.manager.create(ModuleCondensed, {
-    //     moduleCode: 'MA2002',
-    //     moduleLevel: 2,
-    //     title: 'Calculus 1',
-    //   })
-    // )
-
-    console.log(
-      'Express server has started on port 3000. Open http://localhost:3000/users to see results'
-    )
   })
   .catch((error) => console.log(error))
