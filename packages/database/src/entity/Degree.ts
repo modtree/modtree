0a146c2f653df4118b8a24960f0aab4ab81b02d5
weiseng18 <tws.hci@gmail.com>
2022-05-17 19:21:42 +0800

    not working

--- packages/database/src/entity/Degree.ts
+++ packages/database/src/entity/Degree.ts
@@ -6,6 +6,10 @@ import {
   JoinTable,
 } from 'typeorm'
 import { Module } from './Module'
+import { container } from '../data-source'
+import { In } from 'typeorm'
+import { DegreeRepository } from '../repository/Degree'
+import { ModuleRepository } from '../repository/Module'
 
 @Entity({ name: 'degree' })
 export class Degree {
@@ -18,4 +22,70 @@ export class Degree {
 
   @Column()
   title: string
+
+  /**
+   * Adds Modules to a Degree
+   * @param {string[]} moduleCodes
+   */
+  async insertModules(moduleCodes: string[]): Promise<void> {
+    await container(async () => {
+      // find modules to add
+      const modules = await ModuleRepository.find({
+        where: {
+          moduleCode: In(moduleCodes),
+        },
+      })
+
+      // find modules part of current degree
+      const degree = await DegreeRepository.findOne({
+        where: {
+          id: this.id,
+        },
+        relations: ['modules'],
+      })
+
+      // 1. query builder
+      // - many to many error
+      /*
+      await DegreeRepository
+        .createQueryBuilder()
+        .update(Degree)
+        .set({
+          modules,
+        })
+        .where("id = :id", {id: this.id})
+        .execute()
+        */
+
+      // 2. delete and save
+      // - delete not working
+      await DegreeRepository.delete(this.id)
+      await DegreeRepository.delete({
+        id: degree.id
+      })
+      await DegreeRepository.save(degree)
+
+      modules.forEach((one: Module) => {
+        degree.modules.push(one)
+      })
+      // 3. update
+      // - many to many error
+      // await DegreeRepository.update(this.id, degree)
+
+      // 4. save
+      // - duplicate degree
+      // await DegreeRepository.save(degree)
+
+      // 5. save and delete
+      // - delete not working
+      // await DegreeRepository.save(degree)
+      // await DegreeRepository.delete(this.id)
+
+      // 6. extend base entity and save
+      // await degree.save()
+
+      // 7. dont delete just get latest (will require a new created_at column)
+      // await DegreeRepository.save(degree)
+    })
+  }
 }
--- packages/database/tests/degree.test.ts
+++ packages/database/tests/degree.test.ts
@@ -50,3 +50,64 @@ test('Degree.initialize() is successful', async () => {
   expect(props.moduleCodes).toEqual(expect.arrayContaining(moduleCodes))
   expect(moduleCodes).toEqual(expect.arrayContaining(props.moduleCodes))
 })
+
+test('Degree.insertModules', async () => {
+  // 1. Save degree
+  const props = {
+    moduleCodes: [
+      'CS1101S',
+      'CS1231S',
+      'CS2030S',
+      'CS2040S',
+      'CS2100',
+      'CS2103T',
+      'CS2106',
+      'CS2109S',
+      'CS3230',
+    ],
+    title: 'Computer Science',
+  }
+  await container(async () => {
+    await DegreeRepository.initialize(props)
+  })
+
+  const degree = await container(() =>
+    DegreeRepository.findOne({
+      where: {
+        title: props.title,
+      }, relations: ['modules']
+    })
+  )
+
+  expect(degree).toBeDefined()
+  if (!degree) return
+
+  // 2. Add modules to degree
+  const newModuleCodes = ['MA1521', 'MA2001', 'ST2334']
+  await degree.insertModules(newModuleCodes)
+
+  // 3. Search for degree again
+  const degrees = await container(() =>
+    DegreeRepository.find({
+      where: {
+        title: props.title,
+      }, relations: ['modules']
+    })
+  )
+  expect(degrees).toBeDefined()
+  if (!degrees) return
+
+  console.log(degrees)
+  // Inserting modules to the degree should not create a new Degree
+  expect(degrees.length).toEqual(1)
+
+  // 4. Confirm that the insert worked as expected
+  const combinedModuleCodes = props.moduleCodes.concat(newModuleCodes)
+
+  const modules = degrees[0].modules
+
+  // set equality: A = B if A subset of B and B subset of A
+  const moduleCodes = modules.map((one: Module) => one.moduleCode)
+  expect(combinedModuleCodes).toEqual(expect.arrayContaining(moduleCodes))
+  expect(moduleCodes).toEqual(expect.arrayContaining(combinedModuleCodes))
+})
