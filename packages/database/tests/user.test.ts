7895903de5534a843db01e776c4ea21e6e00683e
nguyenvukhang <brew4k@gmail.com>
2022-05-19 01:11:36 +0800

    cut async/await from user test

--- packages/database/tests/user.test.ts
+++ packages/database/tests/user.test.ts
@@ -21,19 +21,18 @@ test('canTakeModule is successful', async () => {
   }
   await UserRepository.initialize(props)
 
-  const res = await endpoint(
-    async () =>
-      await container(async () => {
-        // find user
-        const user = await UserRepository.findOne({
-          where: {
-            username: 'nvkhang',
-          },
-        })
-
-        // user is initialized with MA2001 completed
-        return await user.canTakeModule('MA2101')
+  const res = await endpoint(() =>
+    container(async () => {
+      // find user
+      const user = await UserRepository.findOne({
+        where: {
+          username: 'nvkhang',
+        },
       })
+
+      // user is initialized with MA2001 completed
+      return await user.canTakeModule('MA2101')
+    })
   )
 
   expect(res).toEqual(true)
