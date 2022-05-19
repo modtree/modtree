2182188f2a4d14148339e6ed1bcfc0c49736972f
nguyenvukhang <brew4k@gmail.com>
2022-05-19 18:07:20 +0800

    reduce logs for successful wipe

--- packages/database/src/sql/wipe.ts
+++ packages/database/src/sql/wipe.ts
@@ -1,5 +1,4 @@
 import { createConnection } from 'mysql2/promise'
-import { log } from '../cli'
 import { initConfig } from './config'
 
 export namespace wipe {
@@ -14,17 +13,11 @@ export namespace wipe {
       // drop the database if it exists
       await connection
         .query(`DROP DATABASE ${database};`)
-        .then()
-        .catch(() => {
-          log.gray(`database [${database}] already non-existent`)
-        })
+        .catch(() => `database already non-existent`)
       // create the database again
       await connection
         .query(`CREATE DATABASE ${database};`)
-        .then()
-        .catch(() => {
-          log.gray(`database [${database}] already exists`)
-        })
+        .catch(() => 'database already exists')
       await connection.end()
     })
   }
