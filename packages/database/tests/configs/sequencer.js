3a82b12e24fbee4d8f9ca4bf388fb8e1fe0726c2
nguyenvukhang <brew4k@gmail.com>
2022-05-19 19:18:33 +0800

    remove sequencer.js log

--- packages/database/tests/configs/sequencer.js
+++ packages/database/tests/configs/sequencer.js
@@ -2,7 +2,6 @@ const Sequencer = require('@jest/test-sequencer').default
 // source: https://jestjs.io/docs/configuration#testsequencer-string
 class CustomSequencer extends Sequencer {
   sort(tests) {
-    console.log(typeof tests)
     const copyTests = Array.from(tests)
     /** put base test in front */
     const baseFirst = copyTests.filter((t) => t.path.endsWith('base.test.ts'))
