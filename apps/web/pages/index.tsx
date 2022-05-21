bc167f502116581099252f50b3235db0c5d559a4
nguyenvukhang <brew4k@gmail.com>
2022-05-21 18:04:18 +0800

    fix width

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,16 +1,15 @@
 import { ReactNode, useState } from 'react'
 import Search from '@/components/Search'
-import { H1, H2 } from '@/components/Html'
 import { ModuleCondensed } from 'database'
 import { ResultDisplay } from '@/components/module-search/Results'
 
 export default function SearchPage() {
   const [results, setResults] = useState<ModuleCondensed[]>([])
-  const selectState = useState<string[]>(['meme'])
+  const selectState = useState<string[]>([])
 
   const ResultContainer = (props: { children: ReactNode }) => {
     return (
-      <div className="px-2 py-1 bg-white rounded-md shadow-md">
+      <div className="px-2 py-1 bg-white rounded-md shadow-md w-full">
         {props.children}
       </div>
     )
