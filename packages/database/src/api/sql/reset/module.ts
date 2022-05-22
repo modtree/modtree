c639433f799b35b5b483d951807805546af28877
nguyenvukhang <brew4k@gmail.com>
2022-05-22 21:11:54 +0800

    update yarn reset:m script

--- packages/database/src/api/sql/reset/module.ts
+++ packages/database/src/api/sql/reset/module.ts
@@ -2,8 +2,11 @@ import { remove } from '../../../sql/remove'
 import { config } from '../../../config'
 
 remove.tables(config.database, [
-  'user_modules_done_module',
+  'dag_modules_placed_module',
+  'dag_modules_hidden_module',
+  'dag_modules_hidden_module',
   'user_modules_doing_module',
+  'user_modules_done_module',
   'degree_modules_module',
   'module',
 ])
