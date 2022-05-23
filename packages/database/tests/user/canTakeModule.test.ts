f7e53587a877f53fd710240ed7144b03f5f6d02d
weiseng18 <tws.hci@gmail.com>
2022-05-23 15:20:16 +0800

    split user test

--- packages/database/tests/user/canTakeModule.test.ts
+++ packages/database/tests/user/canTakeModule.test.ts
@@ -1,11 +1,11 @@
-import { container, endpoint, getSource } from '../src/data-source'
-import { Module, User } from '../src/entity'
-import { ModuleRepository, UserRepository } from '../src/repository'
-import { Init } from '../types/modtree'
-import { init } from './init'
-import { setup, importChecks, teardown } from './environment'
+import { container, endpoint, getSource } from '../../src/data-source'
+import { Module, User } from '../../src/entity'
+import { ModuleRepository, UserRepository } from '../../src/repository'
+import { Init } from '../../types/modtree'
+import { init } from '../init'
+import { setup, importChecks, teardown } from '../environment'
 
-const dbName = 'test_user'
+const dbName = 'test_user_canTakeModule'
 const db = getSource(dbName)
 
 beforeAll(() => setup(dbName))
@@ -74,48 +74,5 @@ describe('User.canTakeModule', () => {
 
     await db.destroy()
     await setup(dbName)
-  })
-})
-
-describe('User.eligibleModules', () => {
-  let user: User
-  it('Saves a user', async () => {
-    const props: Init.UserProps = init.emptyUser
-    props.modulesDone.push('MA2001')
-    props.modulesDoing.push('MA2219')
-    await UserRepository(db).initialize(props)
-    const res = await endpoint(db, () =>
-      container(db, async () => {
-        return await UserRepository(db).findOneByUsername(props.username)
-      })
-    )
-    expect(res).toBeDefined()
-    if (!res) return
-    user = res
-  })
-
-  it('Adds only all post-requisites of modules done', async () => {
-    // Get eligible modules
-    const eligibleModules = await container(db, async () => {
-      return await UserRepository(db).eligibleModules(user)
-    })
-    expect(eligibleModules).toBeDefined()
-    if (!eligibleModules) return
-    // Get fulfillRequirements of MA2001 (the only module done)
-    const modules = await endpoint(db, () =>
-      container(db, async () => {
-        // find user
-        return await ModuleRepository(db).findByCodes(['MA2001'])
-      })
-    )
-    expect(modules).toBeDefined()
-    if (!modules) return
-    expect(modules).toBeInstanceOf(Array)
-    expect(modules.length).toEqual(1)
-    const module = modules[0]
-    // Compare module codes
-    const eligibleModuleCodes = eligibleModules.map((one: Module) => one.moduleCode)
-    expect(eligibleModuleCodes.sort()).toEqual(module.fulfillRequirements.sort())
-    expect(eligibleModuleCodes.length).toEqual(module.fulfillRequirements.length)
   })
 })
