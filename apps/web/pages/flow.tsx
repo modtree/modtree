0fe2688d3f976718b22f1793ea7d7aff929b266e
nguyenvukhang <brew4k@gmail.com>
2022-05-23 23:22:49 +0800

    useState

--- apps/web/pages/flow.tsx
+++ apps/web/pages/flow.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -5,6 +6,41 @@ import ReactFlow, {
   Edge,
   ReactFlowProps,
 } from 'react-flow-renderer'
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Node 1' },
+    position: { x: 250, y: 5 },
+    className: 'light',
+    draggable: true,
+    selectable: true,
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 100, y: 100 },
+    className: 'light',
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 400, y: 100 },
+    className: 'light',
+  },
+  {
+    id: '4',
+    data: { label: 'Node 4' },
+    position: { x: 400, y: 200 },
+    className: 'light',
+  },
+]
+
+const initialEdges: Edge[] = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e1-3', source: '1', target: '3' },
+]
 
 function Flow({
   nodes,
@@ -21,50 +57,17 @@ function Flow({
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
     >
-      <MiniMap />
       <Controls />
     </ReactFlow>
   )
 }
 
 export default function Modtree() {
-  const defaultNodes: Node[] = [
-    {
-      id: '1',
-      type: 'input',
-      data: { label: 'Node 1' },
-      position: { x: 250, y: 5 },
-      className: 'light',
-      draggable: true,
-      selectable: true
-    },
-    {
-      id: '2',
-      data: { label: 'Node 2' },
-      position: { x: 100, y: 100 },
-      className: 'light',
-    },
-    {
-      id: '3',
-      data: { label: 'Node 3' },
-      position: { x: 400, y: 100 },
-      className: 'light',
-    },
-    {
-      id: '4',
-      data: { label: 'Node 4' },
-      position: { x: 400, y: 200 },
-      className: 'light',
-    },
-  ]
-
-  const defaultEdges: Edge[] = [
-    { id: 'e1-2', source: '1', target: '2' },
-    { id: 'e1-3', source: '1', target: '3' },
-  ]
+  const [nodes, setNodes] = useState(initialNodes)
+  const [edges, setEdges] = useState(initialEdges)
   return (
-    <div className='h-screen w-screen bg-green-100'>
-      <Flow nodes={defaultNodes} edges={defaultEdges} />
+    <div className="h-screen w-screen bg-green-100">
+      <Flow nodes={initialNodes} edges={initialEdges} />
     </div>
   )
 }
