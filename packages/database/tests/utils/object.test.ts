e44625c0f2543aaca7084577e97d6d478be21ced
weiseng18 <tws.hci@gmail.com>
2022-05-23 15:24:26 +0800

    split utils tests into individual files

---
+++ packages/database/tests/utils/object.test.ts
@@ -0,0 +1,14 @@
+import { ColumnType } from 'typeorm'
+import { createEmpty } from '../../src/utils'
+
+describe('Object.createEmpty', () => {
+  it ('Returns 0 for number', () => {
+    expect(createEmpty(Number)).toEqual(0)
+  })
+  it ('Returns empty string for everything else', () => {
+    const tests: ColumnType[] = [String, 'longblob', 'text', 'json', 'mediumblob']
+    tests.forEach(one => {
+      expect(createEmpty(one)).toEqual('')
+    })
+  })
+})
