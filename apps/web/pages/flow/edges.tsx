549ddf18a9c020960bdea0cc1c251ea3a002f62f
nguyenvukhang <brew4k@gmail.com>
2022-05-23 23:32:38 +0800

    movable nodes

---
+++ apps/web/pages/flow/edges.tsx
@@ -0,0 +1,8 @@
+import { Edge } from "react-flow-renderer"
+
+const initialEdges: Edge[] = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3', animated: true },
+]
+
+export default initialEdges
