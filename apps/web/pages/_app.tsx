96ae7584eec50c333384ed31c2f2277594c4c357
nguyenvukhang <brew4k@gmail.com>
2022-05-24 11:35:41 +0800

    fix root component types

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
-import { AppProps } from 'next/app'
 import { Container } from '../components/Container'
 import { useRouter } from 'next/router'
+import { FC } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: FC
+  pageProps: any
+}) {
   const router = useRouter()
   console.log(router.route)
   const noContainerList = ['/flow']
