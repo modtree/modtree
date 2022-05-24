ac5b4d7d98a37dee586221dc818c12700dd7c931
nguyenvukhang <brew4k@gmail.com>
2022-05-24 21:29:55 +0800

    can now build web + database

--- apps/web/utils/array.ts
+++ apps/web/utils/array.ts
@@ -1,11 +1,13 @@
-
 /**
  * filter, but two ways
  * @param {T[]} array: the array to split in two
  * @param {function} check: the same as filter callback
  * @return {[T[], T[]]} the pass-fail array
  */
-export function partition<T>(array: T[], check: (elem: T) => boolean): [T[], T[]] {
+export function partition<T>(
+  array: T[],
+  check: (elem: T) => boolean
+): [T[], T[]] {
   return array.reduce(
     (result: [pass: T[], fail: T[]], element) => {
       result[check(element) ? 0 : 1].push(element)
@@ -15,4 +17,14 @@ export function partition<T>(array: T[], check: (elem: T) => boolean): [T[], T[]
   )
 }
 
-
+type StrRecord = Record<string, any>
+/**
+ * copies props from source entity to the target entity
+ * @param {StrRecord} source
+ * @param {StrRecord} target
+ */
+export function copy(source: StrRecord, target: StrRecord) {
+  Object.keys(source).forEach(key => {
+    target[key] = source[key]
+  })
+}
