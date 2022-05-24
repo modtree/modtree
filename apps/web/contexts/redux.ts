6bc9897ce076927c3a3d9c71768cb1cdb7130f52
nguyenvukhang <brew4k@gmail.com>
2022-05-24 11:41:31 +0800

    add some absolute imports

--- apps/web/contexts/redux.ts
+++ apps/web/contexts/redux.ts
@@ -1,43 +1,6 @@
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
-/**
- * This is a reducer - a function that takes a current state value and an
- * action object describing "what happened", and returns a new state value.
- * A reducer's function signature is: (state, action) => newState
- *
- * The Redux state should contain only plain JS objects, arrays, and primitives.
- * The root state value is usually an object. It's important that you should
- * not mutate the state object, but return a new object if the state changes.
- *
- * You can use any conditional logic you want in a reducer. In this example,
- * we use a switch statement, but it's not required.
- */
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 }
-    case 'counter/decremented':
-      return { value: state.value - 1 }
-    default:
-      return state
-  }
-}
 
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-export const store = createStore(counterReducer)
-
-// You can use subscribe() to update the UI in response to state changes.
-// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
-// There may be additional use cases where it's helpful to subscribe as well.
-
-store.subscribe(() => console.log(store.getState()))
-
-// The only way to mutate the internal state is to dispatch an action.
-// The actions can be serialized, logged or stored and later replayed.
-store.dispatch({ type: 'counter/incremented' })
-// {value: 1}
-store.dispatch({ type: 'counter/incremented' })
-// {value: 2}
-store.dispatch({ type: 'counter/decremented' })
-// {value: 1}
+export default configureStore({
+  reducer: {}
+})
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,26 +1,25 @@
 import '../styles/globals.css'
-import { Container } from '../components/Container'
+import { Container } from '@/components/Container'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
-import { store } from '../contexts/redux'
+import store from '@/contexts/redux'
+import { Provider } from 'react-redux'
 
-function MyApp({
-  Component,
-  pageProps,
-}: {
-  Component: FC
-  pageProps: any
-}) {
+function MyApp({ Component, pageProps }: { Component: FC; pageProps: any }) {
   const router = useRouter()
   console.log(router.route)
   const noContainerList = ['/flow']
   const noContainer = noContainerList.includes(router.route)
-  return noContainer ? (
-    <Component {...pageProps} />
-  ) : (
-    <Container>
-      <Component {...pageProps} />
-    </Container>
+  return (
+    <Provider store={store}>
+      {noContainer ? (
+        <Component {...pageProps} />
+      ) : (
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      )}
+    </Provider>
   )
 }
 
