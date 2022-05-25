42a03887e7359d8df3405976907ea57f56f2ce31
nguyenvukhang <brew4k@gmail.com>
2022-05-25 21:43:41 +0800

    remove unused types

--- packages/database/src/repository/base/types.d.ts
+++ packages/database/src/repository/base/types.d.ts
@@ -1,10 +1,3 @@
-import { ObjectLiteral, FindOptionsRelations } from 'typeorm'
+import { ObjectLiteral } from 'typeorm'
 
 export type EntityConstructor<T extends ObjectLiteral> = new () => T
-
-export type EntityInstance<T> = EntityConstructor<T> & { id: string }
-
-export type LoadRelations<Entity> = (
-  entity: Entity,
-  relations: FindOptionsRelations<Entity>
-) => Promise<void>
