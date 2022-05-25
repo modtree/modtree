a44f8b8df648dd0860cda5ac80fc64c33ce97cdb
nguyenvukhang <brew4k@gmail.com>
2022-05-25 21:36:21 +0800

    import repository.d.ts

---
+++ packages/database/types/repository.d.ts
@@ -0,0 +1,82 @@
+import { Repository } from 'typeorm'
+import { Init, DAGProps, UserProps, DegreeProps } from './modtree'
+import type { DAG, User, Degree, Module, ModuleCondensed } from '../src/entity'
+import type { Module as NM, ModuleCondensed as NMC } from './nusmods'
+import { ObjectLiteral, FindOptionsRelations } from 'typeorm'
+
+type EntityConstructor<T extends ObjectLiteral> = new () => T
+
+type EntityInstance<T> = EntityConstructor<T> & { id: string }
+
+type LoadRelations<Entity> = (
+  entity: Entity,
+  relations: FindOptionsRelations<Entity>
+) => Promise<void>
+
+type DeleteAll = () => Promise<void>
+
+export namespace base {
+
+}
+
+/**
+ * BaseRepository, but for now only in types
+ * it is a interface that will be extended to form the final Repositories of modtree
+ */
+interface BaseRepo<Entity, BuildProps, InitProps = BuildProps>
+  extends Repository<Entity> {
+  build(props: BuildProps): Entity
+  initialize?(props: InitProps): Promise<void>
+}
+
+export interface DAGRepository extends BaseRepo<DAG, DAGProps, Init.DAGProps> {
+  toggleModule(dag: DAG, moduleCode: string): Promise<void>
+  loadRelations: LoadRelations<DAG>
+  findOneByUserAndDegreeId(userId: string, degreeId: string): Promise<DAG>
+  findManyByUserAndDegreeId(
+    userId: string,
+    degreeId: string
+  ): Promise<[DAG[], number]>
+}
+
+export interface UserRepository
+  extends BaseRepo<User, UserProps, Init.UserProps> {
+  build(props: UserProps): User
+  initialize(props: Init.UserProps): Promise<void>
+  canTakeModule(user: User, moduleCode: string): Promise<boolean | void>
+  loadRelations: LoadRelations<User>
+  findOneByUsername(username: string): Promise<User>
+  eligibleModules(user: User): Promise<Module[] | void>
+  findOneById(id: string): Promise<User>
+}
+
+export interface DegreeRepository
+  extends BaseRepo<Degree, DegreeProps, Init.DegreeProps> {
+  build(props: DegreeProps): Degree
+  initialize(props: Init.DegreeProps): Promise<void>
+  insertModules(degree: Degree, moduleCodes: string[]): Promise<void>
+  loadRelations: LoadRelations<Degree>
+  findOneByTitle(title: string): Promise<Degree>
+  findOneById(id: string): Promise<Degree>
+}
+
+export interface ModuleRepository extends BaseRepo<Module, NM> {
+  build(props: NM): Module
+  get(): Promise<Module[]>
+  fetchOne(moduleCode: string): Promise<Module>
+  getCodes(): Promise<string[]>
+  pull(): Promise<Module[]>
+  findByFaculty(faculty: string): Promise<Module[]>
+  loadRelations: LoadRelations<Module>
+  findByCodes(moduleCodes: string[]): Promise<Module[]>
+}
+
+export interface ModuleCondensedRepository
+  extends BaseRepo<ModuleCondensed, NMC> {
+  build(props: NMC): ModuleCondensed
+  pull(): Promise<ModuleCondensed[]>
+  fetch(): Promise<ModuleCondensed[]>
+  getCodes(): Promise<string[]>
+  loadRelations: LoadRelations<ModuleCondensed>
+  deleteAll: DeleteAll
+}
