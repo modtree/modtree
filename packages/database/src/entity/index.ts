e33e46a666e114507fed9e417abb6d0e639c4410
nguyenvukhang <brew4k@gmail.com>
2022-05-25 22:09:34 +0800

    retire types in entity/index

--- packages/database/src/entity/index.ts
+++ packages/database/src/entity/index.ts
@@ -1,16 +1,6 @@
-import { ModuleCondensed } from './ModuleCondensed'
-import { Module } from './Module'
-import { Degree } from './Degree'
-import { User } from './User'
-import { DAG } from './DAG'
-import { Base } from './Base'
-
-export type ModtreeEntity = Module | ModuleCondensed | Degree | User | DAG
-export type ModtreeEntityConstructor =
-  | typeof Module
-  | typeof ModuleCondensed
-  | typeof Degree
-  | typeof User
-  | typeof DAG
-
-export { Module, ModuleCondensed, Degree, User, DAG, Base }
+export { ModuleCondensed } from './ModuleCondensed'
+export { Module } from './Module'
+export { Degree } from './Degree'
+export { User } from './User'
+export { DAG } from './DAG'
+export { Base } from './Base'
