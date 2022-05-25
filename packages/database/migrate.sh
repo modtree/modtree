3a941b3795944fa06d399e19dc046a9c13481a57
nguyenvukhang <brew4k@gmail.com>
2022-05-25 10:47:36 +0800

    wrote migration script

--- packages/database/migrate.sh
+++ packages/database/migrate.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
-# get this script's filename
-SCRIPTNAME=`basename "$0"`
-# get this script's parent directory
-DATABASE_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+function setup() {
+  # get this script's filename
+  SCRIPTNAME=`basename "$0"`
+  # get this script's parent directory
+  DATABASE_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+  # where all existing migrations are
+  MIGRATION_DIR=$DATABASE_ROOT/src/migrations
+  # create temporary working dir
+  TMP=$MIGRATION_DIR/tmp
+  # obliterate tmp dir upon early exit
+  function cleanup() {
+    rm -rf $TMP
+  }
+  trap cleanup EXIT
+  # clear the temporary directory
+  rm -rf $TMP
+  mkdir -p $TMP
+  # grab the date
+  DATE=$(date +'%Y-%m-%d_%R')
+}
 
-MIGRATION_DIR=$DATABASE_ROOT/src/migrations
-NAME="scripted-migration"
-# NODE_ENV=migrate yarn mc
-RESTORE_SOURCE=.test.sql yarn restore:file
-# SYNCHRONIZE=true yarn typeorm_ds migration:generate $MIGRATION_DIR/
-# mv $MIGRATION_DIR/*$NAME.ts $MIGRATION_DIR/$NAME.ts
+function migrate() {
+  RESTORE_SOURCE=$RESTORE_SOURCE yarn restore:file
+  SYNCHRONIZE=true yarn typeorm_ds migration:generate $TMP/$NAME
+  mv $TMP/* $MIGRATION_DIR/${NAME}_${DATE}.ts
+}
 
+# CONFIGS
+RESTORE_SOURCE=.test.sql
+NAME="migration"
+
+# MAIN
+setup
+migrate
