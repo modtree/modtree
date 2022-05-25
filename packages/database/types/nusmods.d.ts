d82cc5acb04acbee186c2d3eed0cdc629316e58e
nguyenvukhang <brew4k@gmail.com>
2022-05-25 21:23:39 +0800

    clean out nusmods.d.ts

--- packages/database/types/nusmods.d.ts
+++ packages/database/types/nusmods.d.ts
@@ -40,22 +40,9 @@ export type Day =
   | 'Saturday'
   | 'Sunday'
 
-export const WorkingDaysOfWeek: Day[] = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-]
-
-export const DaysOfWeek: Day[] = [...WorkingDaysOfWeek, 'Sunday']
-
 export type Time = 'Morning' | 'Afternoon' | 'Evening'
-export const TimesOfDay: Time[] = ['Morning', 'Afternoon', 'Evening']
 
 export type ModuleLevel = 1 | 2 | 3 | 4 | 5 | 6 | 8
-export const Semesters = [1, 2, 3, 4]
 
 export type WorkloadComponent =
   | 'Lecture'
