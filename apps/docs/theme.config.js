286e2542d63147c8bd06b8cbab1e8c20b057695e
nguyenvukhang <brew4k@gmail.com>
2022-05-26 00:48:24 +0800

    cleanup footer

--- apps/docs/theme.config.js
+++ apps/docs/theme.config.js
@@ -1,7 +1,7 @@
 // theme.config.js
 export default {
-  projectLink: "https://github.com/modtree/modtree", // GitHub link in the navbar
-  titleSuffix: " – modtree",
+  projectLink: 'https://github.com/modtree/modtree', // GitHub link in the navbar
+  titleSuffix: ' – modtree',
   nextLinks: true,
   prevLinks: true,
   search: false,
@@ -9,17 +9,13 @@ export default {
   darkMode: true,
   footer: true,
   footerText: `MIT ${new Date().getFullYear()} © modtree.`,
-  footerEditLink: 'Edit this page on GitHub',
-  logo: (
-    <>
-      <img src="/logo.png" width={160} />
-    </>
-  ),
+  footerEditLink: '',
+  logo: <img src="/logo.png" width={160} />,
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Nextra: the next docs builder" />
-      <meta name="og:title" content="Nextra: the next docs builder" />
+      <meta name="description" content="modtree: the graphic degree planner" />
+      <meta name="og:title" content="modtree: the graphic degree planner" />
     </>
   ),
-};
+}
