b6bbf35edb5feca3d5bde9d1bfaef1b87ea8550b
nguyenvukhang <brew4k@gmail.com>
2022-05-25 21:35:42 +0800

    fix User and Degree imports

--- packages/database/types/modtree.d.ts
+++ packages/database/types/modtree.d.ts
@@ -1,4 +1,4 @@
-import type { Module } from "../src/entity"
+import type { Module, Degree, User } from "../src/entity"
 
 export type ModtreeFunction<T> = () => Promise<T>
 type Data = string | number | boolean
