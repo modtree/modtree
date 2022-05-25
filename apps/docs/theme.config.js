a8674966c5a343aecc7f98276df1afef6e9d057a
nguyenvukhang <brew4k@gmail.com>
2022-05-25 22:51:03 +0800

    import working nextra

---
+++ apps/docs/theme.config.js
@@ -0,0 +1,26 @@
+const Search = require('./components/docsearch').default
+// theme.config.js
+export default {
+  projectLink: "https://github.com/modtree/modtree", // GitHub link in the navbar
+  titleSuffix: " – modtree",
+  nextLinks: true,
+  prevLinks: true,
+  search: false,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © modtree.`,
+  footerEditLink: 'Edit this page on GitHub',
+  logo: (
+    <>
+      <img src="/logo.png" width={160} />
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
+};
