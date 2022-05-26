afd467163f7f1789a7ccd683c1b2babc5f6f5411
nguyenvukhang <brew4k@gmail.com>
2022-05-26 15:07:05 +0800

    made features global

--- apps/turbo-docs/components/features.js
+++ apps/turbo-docs/components/features.js
@@ -1,70 +1,42 @@
 import {
+  ShareIcon,
   ArrowsExpandIcon,
   BeakerIcon,
-  ChartBarIcon,
-  ChartPieIcon,
-  ChipIcon,
+  BellIcon,
   CloudUploadIcon,
   FingerPrintIcon,
-  LightningBoltIcon,
-  RefreshIcon,
 } from "@heroicons/react/outline";
 
-const features = [
+export const features = [
   {
-    name: "Incremental builds",
-    description: `Building once is painful enough, Turborepo will remember what you've built and skip the stuff that's already been computed.`,
-    icon: RefreshIcon,
+    name: "Smart Reminders",
+    description: `Modtree will remind you to take pre-requisites so you don't have to check yourself.`,
+    icon: BellIcon,
   },
   {
-    name: "Content-aware hashing",
-    description: `Turborepo looks at the contents of your files, not timestamps to figure out what needs to be built.`,
+    name: "Easy Authentication",
+    description: `Modtree offers an option of passwordless, OTP-only authentication.`,
     icon: FingerPrintIcon,
   },
   {
-    name: "Cloud caching",
-    description: `Share a cloud build cache with your teammates and CI/CD for even faster builds.`,
+    name: "Remote Storage",
+    description: `Save your degree and module plans online.`,
     icon: CloudUploadIcon,
   },
   {
-    name: "Parallel execution",
-    description: `Execute builds using every core at maximum parallelism without wasting idle CPUs.`,
-    icon: LightningBoltIcon,
-  },
-  {
-    name: "Task pipelines",
-    description: `Define the relationships between your tasks and then let Turborepo optimize what to build and when.`,
-    icon: ArrowsExpandIcon,
-  },
-  {
-    name: "Zero runtime overhead",
-    description: `Turborepo doesn't interfere with your runtime code or touch your sourcemaps. It does what it does and then gets out of your way.`,
-    icon: ChipIcon,
-  },
-  // {
-  //   name: 'Package manager agnostic',
-  //   description: `Turborepo works with Yarn v1, Yarn v2, npm, and pnpm workspaces.`,
-  //   icon: LightningBoltIcon,
-  // },
-  // {
-  //   name: 'Focused installs',
-  //   description: `Only install the dependencies you actually need. Works perfectly with Docker layer caching.`,
-  //   icon: DownloadIcon,
-  // },
-  {
-    name: "Pruned subsets",
-    description: `Speed up PaaS deploys by generating a subset of your monorepo with only what's needed to build a specific target.`,
-    icon: ChartPieIcon,
-  },
-  {
-    name: "JSON configuration",
-    description: `Reduce complexity through convention. Fan out configuration with just a few lines of JSON.`,
+    name: "Switch it up",
+    description: `Efficiently trial-and-error many possible ways of completing your degree.`,
     icon: BeakerIcon,
   },
   {
-    name: `Profile in your browser`,
-    description: `Generate build profiles and import them in Chrome or Edge to understand which tasks are taking the longest.`,
-    icon: ChartBarIcon,
+    name: "Power of overview",
+    description: `Modtree lets you visualize your entire degree all the way through, while giving you the flexibility to make it truly your own.`,
+    icon: ArrowsExpandIcon,
+  },
+  {
+    name: "Community templates",
+    description: `Pick from one of many existing templates that your friends created to jumpstart your degree planning.`,
+    icon: ShareIcon,
   },
 ];
 
@@ -75,7 +47,7 @@ function Features() {
         {features.map(({ icon: Icon, ...feature }, i) => (
           <div
             className="flex items-center space-x-4"
-            key={feature.name.split(" ").join("-")}
+            key={i}
           >
             <div>
               <Icon
