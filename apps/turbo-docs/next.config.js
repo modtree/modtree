3fe2ed886363af1405b3150da8ac615720d27c9c
nguyenvukhang <brew4k@gmail.com>
2022-05-26 11:48:33 +0800

    naively forked turboerepo docs

---
+++ apps/turbo-docs/next.config.js
@@ -0,0 +1,47 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  unstable_flexsearch: true,
+  unstable_staticImage: true,
+});
+
+module.exports = withNextra({
+  // reactStrictMode: true,
+  experiments: {
+    esmExternals: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/usage",
+        destination: "/reference/command-line-reference",
+        permanent: true,
+      },
+      {
+        source: "/discord{/}?",
+        permanent: true,
+        destination: "https://discord.gg/sSzyjxvbf5",
+      },
+      {
+        source: "/docs/changelog",
+        permanent: true,
+        destination: "https://github.com/vercel/turborepo/releases",
+      },
+      {
+        source: "/docs/guides/complimentary-tools",
+        permanent: true,
+        destination: "/docs/guides/monorepo-tools",
+      },
+      {
+        source: "/docs/guides/continuous-integration",
+        permanent: true,
+        destination: "/docs/ci",
+      },
+      {
+        source: "/docs/features/:path*",
+        permanent: true,
+        destination: "/docs/core-concepts/:path*",
+      },
+    ];
+  },
+});
