7bd5597d3b37fc48a40e8202ce0d096ce38ac93d
nguyenvukhang <brew4k@gmail.com>
2022-05-27 18:45:22 +0800

    yarn module:get

--- packages/database/src/api/module/get-codes.ts
+++ packages/database/src/api/module/get-codes.ts
@@ -3,4 +3,4 @@ import { container } from '../..'
 import { db } from '../../config'
 import { analyze } from '../analyze'
 
-analyze(() => container(db, () => ModuleRepository(db).get()))
+analyze(() => container(db, () => ModuleRepository(db).getCodes()))
