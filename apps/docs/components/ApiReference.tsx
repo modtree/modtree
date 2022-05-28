62647740247838e826c5644add2b6396956192f8
nguyenvukhang <brew4k@gmail.com>
2022-05-28 20:13:00 +0800

    type-checked abstraction of docs data

--- apps/docs/components/ApiReference.tsx
+++ apps/docs/components/ApiReference.tsx
@@ -1,10 +1,34 @@
-import ModuleCondensedDocs from '../data/module-condensed'
+import ModuleCondensed from '@/data/module-condensed'
+import Module from '@/data/module'
+import Degree from '@/data/degree'
+import DAG from '@/data/dag'
+import User from '@/data/user'
 import Method from './Method'
 
-const ModuleCondensed: Partial<Record<string, any>> = {}
+type EntityName = 'Module' | 'ModuleCondensed' | 'Degree' | 'DAG' | 'User'
 
-Object.keys(ModuleCondensedDocs).forEach((key) => {
-  ModuleCondensed[key] = () => <Method {...ModuleCondensedDocs[key]} />
+type EntityMethodDocs = Record<string, () => JSX.Element>
+
+const data: Record<EntityName, Record<string, MethodProps>> = {
+  Module,
+  ModuleCondensed,
+  Degree,
+  DAG,
+  User,
+}
+
+const API: Record<EntityName, EntityMethodDocs> = {
+  Module: {},
+  ModuleCondensed: {},
+  Degree: {},
+  DAG: {},
+  User: {},
+}
+
+Object.keys(data).forEach((entity: EntityName) => {
+  Object.keys(data[entity]).forEach((method) => {
+    API[entity][method] = () => <Method {...data[entity][method]} />
+  })
 })
 
-export { ModuleCondensed }
+export default API
