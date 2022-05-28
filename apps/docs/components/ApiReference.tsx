87858f42d4c158ce3172dd35520d801640354027
nguyenvukhang <brew4k@gmail.com>
2022-05-28 20:16:38 +0800

    comments

--- apps/docs/components/ApiReference.tsx
+++ apps/docs/components/ApiReference.tsx
@@ -9,6 +9,10 @@ type EntityName = 'Module' | 'ModuleCondensed' | 'Degree' | 'DAG' | 'User'
 
 type EntityMethodDocs = Record<string, () => JSX.Element>
 
+/**
+ * by the power of JavaScript colon omission,
+ * this contains all the imported docs
+ */
 const data: Record<EntityName, Record<string, MethodProps>> = {
   Module,
   ModuleCondensed,
@@ -17,6 +21,12 @@ const data: Record<EntityName, Record<string, MethodProps>> = {
   User,
 }
 
+/**
+ * This will be the exported variable.
+ * Documentation components will be accessible, for instance, via
+ * <API.ModuleCondensed.get />
+ * in the mdx files that import this variable.
+ */
 const API: Record<EntityName, EntityMethodDocs> = {
   Module: {},
   ModuleCondensed: {},
@@ -25,6 +35,7 @@ const API: Record<EntityName, EntityMethodDocs> = {
   User: {},
 }
 
+/** The raw-data to React Component mapper */
 Object.keys(data).forEach((entity: EntityName) => {
   Object.keys(data[entity]).forEach((method) => {
     API[entity][method] = () => <Method {...data[entity][method]} />
