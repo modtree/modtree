13d39723567780c228cdb3a66b6f502b980c274e
nguyenvukhang <brew4k@gmail.com>
2022-06-01 14:51:14 +0800

    add an incomplete query test for module condensed

--- packages/database/tests/server/module-condensed.test.ts
+++ packages/database/tests/server/module-condensed.test.ts
@@ -13,3 +13,19 @@ test('It should get a summary of CS1010S', async () => {
     })
   expect(res).toBeDefined()
 })
+
+test('It should get a search results of CS', async () => {
+  const res = await axios
+    .get('http://localhost:8080/modules/CS')
+    .then((res) => {
+      const results: any[] = res.data
+      expect(results.length).toBeGreaterThan(10)
+      results.forEach(x => {
+        expect(typeof x.title).toStrictEqual('string')
+        expect(typeof x.moduleCode).toStrictEqual('string')
+        expect(typeof x.moduleLevel).toStrictEqual('number')
+      })
+      return true
+    })
+  expect(res).toBeDefined()
+})
