31b533630dfd1365e9722add77acf88dfdc9f21c
weiseng18 <tws.hci@gmail.com>
2022-06-01 16:18:20 +0800

    prettier

--- packages/database/script/index.ts
+++ packages/database/script/index.ts
@@ -1,27 +1,27 @@
 import axios from 'axios'
 
 axios.post('http://localhost:8080/user/create', {
-    displayName: 'Tan Wei Seng',
-    username: 'weiseng',
-    email: 'tanweiseng18@gmail.com',
-    modulesDone: ['CS1101S', 'CS2030S', 'CS1231S', 'CS2040S'],
-    modulesDoing: [],
-    matriculationYear: 2021,
-    graduationYear: 2025,
-    graduationSemester: 2,
+  displayName: 'Tan Wei Seng',
+  username: 'weiseng',
+  email: 'tanweiseng18@gmail.com',
+  modulesDone: ['CS1101S', 'CS2030S', 'CS1231S', 'CS2040S'],
+  modulesDoing: [],
+  matriculationYear: 2021,
+  graduationYear: 2025,
+  graduationSemester: 2,
 }).then((msg) => {
-    console.log(msg)
+  console.log(msg)
 })
 
 axios.post('http://localhost:8080/user/create', {
-    displayName: 'Nguyen Vu Khang',
-    username: 'nguyenvukhang',
-    email: 'brew4k@gmail.com',
-    modulesDone: ['MA2001', 'MA2002', 'CS1010S', 'MA2219'],
-    modulesDoing: [],
-    matriculationYear: 2021,
-    graduationYear: 2025,
-    graduationSemester: 2,
+  displayName: 'Nguyen Vu Khang',
+  username: 'nguyenvukhang',
+  email: 'brew4k@gmail.com',
+  modulesDone: ['MA2001', 'MA2002', 'CS1010S', 'MA2219'],
+  modulesDoing: [],
+  matriculationYear: 2021,
+  graduationYear: 2025,
+  graduationSemester: 2,
 }).then((msg) => {
-    console.log(msg)
+  console.log(msg)
 })
