ed891be376066274ed569c8a98733360d79db2d1
weiseng18 <tws.hci@gmail.com>
2022-06-16 13:48:49 +0800

    fix: remove additional getBaseUserData call

--- apps/web/components/modals/UserProfile/ModulesDone.tsx
+++ apps/web/components/modals/UserProfile/ModulesDone.tsx
@@ -1,19 +1,11 @@
-import { useUser } from '@auth0/nextjs-auth0'
-import { useDispatch, useSelector } from 'react-redux'
-import { getBaseUserData, UserState } from '@/store/base'
+import { useSelector } from 'react-redux'
+import { UserState } from '@/store/base'
 import { ModtreeApiResponse } from '@modtree/types'
-import { useEffect } from 'react'
 
 export default function ModulesDone() {
-  const { user } = useUser()
-  const dispatch = useDispatch()
   const reduxUser = useSelector<UserState, ModtreeApiResponse.User>(
     (state) => state.base.user
   )
-
-  useEffect(() => {
-    if (user && user.email) getBaseUserData(dispatch, user.email)
-  }, [user])
 
   return (
     <div className="w-full h-full">
