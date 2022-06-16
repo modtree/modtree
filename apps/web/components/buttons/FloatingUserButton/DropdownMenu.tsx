cfcc30580ced2acff34b1fa16dce7688f80e6979
nguyenvukhang <brew4k@gmail.com>
2022-06-16 15:31:45 +0800

    fix: build

--- apps/web/components/buttons/FloatingUserButton/DropdownMenu.tsx
+++ apps/web/components/buttons/FloatingUserButton/DropdownMenu.tsx
@@ -2,26 +2,32 @@ import { Menu, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import { FC, ForwardedRef, forwardRef, Fragment, ReactElement } from 'react'
 
-export const MenuLink = forwardRef(
-  (
-    props: {
-      href: string
-      children: string
-      className: string
-      onClick: () => void
-    },
-    ref: ForwardedRef<HTMLAnchorElement>
-  ) => {
-    const { href, children, className, onClick } = props
-    return (
-      <Link href={href} passHref>
-        <a className={className} onClick={onClick} ref={ref}>
-          {children}
-        </a>
-      </Link>
-    )
-  }
-)
+function getMenuLink() {
+  const MenuLink = forwardRef(
+    (
+      props: {
+        href: string
+        children: string
+        className: string
+        onClick: () => void
+      },
+      ref: ForwardedRef<HTMLAnchorElement>
+    ) => {
+      const { href, children, className, onClick } = props
+      return (
+        <Link href={href} passHref>
+          <a className={className} onClick={onClick} ref={ref}>
+            {children}
+          </a>
+        </Link>
+      )
+    }
+  )
+  MenuLink.displayName = 'MenuLink'
+  return MenuLink
+}
+
+export const MenuLink = getMenuLink()
 
 export default function DropdownMenu(props: {
   TriggerButton: FC
