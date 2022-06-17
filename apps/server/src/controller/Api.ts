08080cb645abda9e9fdef00216b4cc1606eccd36
nguyenvukhang <brew4k@gmail.com>
2022-06-17 11:20:03 +0800

    feat(frontend): link user login to api login

---
+++ apps/server/src/controller/Api.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import { flatten } from '@modtree/utils'
+import { db } from '@modtree/typeorm-config'
+import { Api } from '@modtree/repo-api'
+import { validate } from './base'
+
+/** Graph API controller */
+export class ApiController {
+  private api = new Api(db)
+
+  /**
+   * handles user login
+   *
+   * @param {Request} req
+   * @param {Response} res
+   */
+  async userLogin(req: Request, res: Response) {
+    if (!validate(req, res)) return
+    const { authZeroId, email } = req.body
+    return this.api
+      .userLogin(authZeroId, email)
+      .then((user) => {
+        res.json(flatten.user(user))
+      })
+      .catch(() => {
+        res.status(500)
+      })
+  }
+}
--- libs/postman/user/list.ts
+++ libs/postman/user/list.ts
@@ -4,6 +4,8 @@ postman.get('user/list').then((res) => {
   const pretty = res.map((u: any) => ({
     id: u.id,
     user: u.username,
+    graphs: u.savedGraphs,
+    degrees: u.savedDegrees,
   }))
   console.log(pretty)
 })
