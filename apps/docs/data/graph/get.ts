f15dc900f6b2adfd35ee2f85e0a15915cfe4485c
weiseng18 <tws.hci@gmail.com>
2022-06-17 13:04:44 +0800

    feat: update GET /graph/{graphId} example response

--- apps/docs/data/graph/get.ts
+++ apps/docs/data/graph/get.ts
@@ -6,6 +6,25 @@ const fulfilled = {
   degree: 'Mathematics',
   modulesPlaced: ['MA2001', 'MA2002'],
   modulesHidden: ['CS2040S'],
+  flowNodes: [
+    {
+      moduleCode: 'CS1010',
+      title: 'Programming Methodology',
+      position: { x: 100, y: 200 },
+    },
+    {
+      moduleCode: 'CS2030S',
+      title: 'Programming Methodology II',
+      position: { x: 450, y: 100 },
+    },
+  ],
+  flowEdges: [
+    {
+      id: 'CS1010-CS2030S',
+      source: 'CS1010',
+      target: 'CS2030S',
+    },
+  ],
 }
 
 export const get: MethodProps = {
