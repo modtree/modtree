20d2b03795709c8a7909fbecc591ccda4c67f580
weiseng18 <tws.hci@gmail.com>
2022-06-17 13:04:13 +0800

    feat: test generated schema on GET /graph/{graphId}

--- apps/docs/data/graph/get.ts
+++ apps/docs/data/graph/get.ts
@@ -1,3 +1,13 @@
+import { generateSchema } from 'utils'
+
+const fulfilled = {
+  id: 'ab0b6e0d-19cc-4c2e-ad7b-7e840cb04f38',
+  user: 'nguyenvukhang',
+  degree: 'Mathematics',
+  modulesPlaced: ['MA2001', 'MA2002'],
+  modulesHidden: ['CS2040S'],
+}
+
 export const get: MethodProps = {
   method: 'Get a Graph',
   description: 'Retrieve information about one Graph.',
@@ -14,19 +24,7 @@ export const get: MethodProps = {
     ],
   },
   response: {
-    fulfilled: {
-      id: 'ab0b6e0d-19cc-4c2e-ad7b-7e840cb04f38',
-      user: 'nguyenvukhang',
-      degree: 'Mathematics',
-      modulesPlaced: ['MA2001', 'MA2002'],
-      modulesHidden: ['CS2040S'],
-    },
-    schema: {
-      id: 'string',
-      user: 'string',
-      degree: 'string',
-      modulesPlaced: 'string[]',
-      modulesHidden: 'string[]',
-    },
+    fulfilled,
+    schema: generateSchema(fulfilled),
   },
 }
