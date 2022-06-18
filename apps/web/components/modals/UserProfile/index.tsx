337ee6bff5ebb85c6af3f98b56098c0bc60f6ff6
nguyenvukhang <brew4k@gmail.com>
2022-06-18 11:52:08 +0800

    feat: implement sidebar buttons

--- apps/web/components/modals/UserProfile/index.tsx
+++ apps/web/components/modals/UserProfile/index.tsx
@@ -2,15 +2,34 @@ import { Panel } from '../base'
 import { hideUserProfile, ModalState } from '@/store/modal'
 import { useSelector } from 'react-redux'
 import SavedGraphs from './saved-graphs'
+import { Tab } from '@headlessui/react'
+
+const SidebarButton = (props: { children: string }) => {
+  return <Tab className="text-left">{props.children}</Tab>
+}
+
+const Sidebar = () => {
+  return (
+    <Tab.List className="w-48 bg-blue-100 flex flex-col">
+      <SidebarButton>Public profile</SidebarButton>
+      <SidebarButton>Account</SidebarButton>
+      <SidebarButton>SidebarButton 3</SidebarButton>
+    </Tab.List>
+  )
+}
 
 const Contents = () => {
   return (
-    <div className="bg-green-100">
-      <SavedGraphs />
-      <SavedGraphs />
-      <SavedGraphs />
-      <SavedGraphs />
-    </div>
+    <Tab.Group as="div">
+      <div className="bg-green-100 flex flex-row">
+        <Sidebar />
+        <Tab.Panels>
+          <Tab.Panel>Content 1</Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+          <Tab.Panel>Content 3</Tab.Panel>
+        </Tab.Panels>
+      </div>
+    </Tab.Group>
   )
 }
 
