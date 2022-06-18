35daa185c085411268870ae74561186690a4c3ef
nguyenvukhang <brew4k@gmail.com>
2022-06-18 12:40:30 +0800

    feat: add an input component

--- apps/web/components/modals/UserProfile/html.tsx
+++ apps/web/components/modals/UserProfile/html.tsx
@@ -1,8 +1,10 @@
+import { UseState } from '@modtree/types'
+
 const base = ''
 const className = {
   h1: `text-xl mb-3 ${base}`,
   h2: `text-lg mb-3 ${base}`,
-  h3: `text-sm font-semibold mb-3 ${base}`,
+  h3: `text-sm font-semibold mb-2 text-gray-800 ${base}`,
 }
 
 export const H1 = (props: { children: string }) => (
@@ -23,3 +25,15 @@ export const H3 = (props: { children: string }) => (
     <h3 className={className.h3}>{props.children}</h3>
   </>
 )
+
+export const Input = (props: { state: UseState<string> }) => {
+  const [value, setValue] = props.state
+  const interact = 'focus:outline-none focus:border-modtree-200'
+  return (
+    <input
+      className={`w-full text-sm py-1 px-2 border rounded-md ${interact}`}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  )
+}
--- apps/web/components/modals/UserProfile/public-profile.tsx
+++ apps/web/components/modals/UserProfile/public-profile.tsx
@@ -1,10 +1,17 @@
-import { H1, H3 } from './html'
+import { useUser } from '@/utils/auth0'
+import { useState } from 'react'
+import { H1, H3, Input } from './html'
 
 export default function PublicProfile() {
+  const { user } = useUser()
+  const state = {
+    name: useState<string>(user.nickname),
+  }
   return (
     <div>
       <H1>Public profile</H1>
       <H3>Name</H3>
+      <Input state={state.name} />
     </div>
   )
 }
