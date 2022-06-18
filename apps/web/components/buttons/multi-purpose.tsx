edd1e2ddd5a4be26128564f65123207b98095471
nguyenvukhang <brew4k@gmail.com>
2022-06-18 18:32:56 +0800

    feat: use functional components to write button

--- apps/web/components/buttons/multi-purpose.tsx
+++ apps/web/components/buttons/multi-purpose.tsx
@@ -1,44 +1,41 @@
-import { Component, HTMLAttributes } from 'react'
+import { HTMLAttributes } from 'react'
 import { flatten } from '@/utils/tailwind'
 
 type ButtonProps = HTMLAttributes<HTMLDivElement> & {
   color?: 'red' | 'blue' | 'green' | 'gray'
 }
 
-export class Button extends Component<ButtonProps> {
-  private color: string
-  /**
-   * applies to all buttons
-   */
-  private base =
-    'h-8 border text-sm font-medium px-4 flex justify-center items-center ' +
-    'rounded-md hover:bg-opacity-90 active:bg-opacity-80 whitespace-nowrap'
-  /**
-   * exists because tailwind only takes literals
-   */
-  private colorMap = {
-    green: ['bg-green-600', 'border-green-700/80', 'text-neutral-50'],
-    blue: ['bg-blue-600', 'border-blue-700', 'text-neutral-50'],
-    gray: ['bg-gray-100', 'border-gray-300', 'text-gray-800'],
-    red: [
-      'bg-gray-100',
-      'border-gray-300',
-      'text-red-600',
-      'hover:bg-red-700',
-      'hover:text-white',
-      'hover:border-red-700',
-    ],
-  }
-  constructor(props: ButtonProps) {
-    super(props)
-    this.color = props.color || 'gray'
-  }
-  render() {
-    const flat = flatten(...this.colorMap[this.color])
-    return (
-      <div className="inline-block cursor-pointer bg-black rounded-lg">
-        <div className={`${this.base} ${flat}`} {...this.props} />
-      </div>
-    )
-  }
+/**
+ * applies to all buttons
+ */
+const base =
+  'h-8 border text-sm font-medium px-4 flex justify-center items-center ' +
+  'rounded-md hover:bg-opacity-90 active:bg-opacity-80 whitespace-nowrap'
+
+/**
+ * exists because tailwind only takes literals
+ */
+const colorMap = {
+  green: ['bg-green-600', 'border-green-700/80', 'text-neutral-50'],
+  blue: ['bg-blue-600', 'border-blue-700', 'text-neutral-50'],
+  gray: ['bg-gray-100', 'border-gray-300', 'text-gray-800'],
+  red: [
+    'bg-gray-100',
+    'border-gray-300',
+    'text-red-600',
+    'hover:bg-red-700',
+    'hover:text-white',
+    'hover:border-red-700',
+  ],
+}
+
+export function Button(props: ButtonProps) {
+  const color = props.color || 'gray'
+  const accents = flatten(...colorMap[color])
+  const { className, ...rest } = props
+  return (
+    <div className="inline-block cursor-pointer bg-black rounded-lg">
+      <div className={`${base} ${accents}`} {...rest} />
+    </div>
+  )
 }
