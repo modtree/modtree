462a23c57e795a478b4f7c3ba72b8f06ceb3b52e
weiseng18 <tws.hci@gmail.com>
2022-06-19 23:56:07 +0800

    test: Degree.delete

---
+++ libs/repo-degree/test/delete.test.ts
@@ -0,0 +1,79 @@
+import { User, Graph } from '@modtree/entity'
+import { setup, teardown, Repo, t, init } from '@modtree/test-env'
+import { oneUp } from '@modtree/utils'
+import { getSource } from '@modtree/typeorm-config'
+import { DegreeRepository } from '../src'
+import { UserRepository } from '@modtree/repo-user'
+import { GraphRepository } from '@modtree/repo-graph'
+
+const dbName = oneUp(__filename)
+const db = getSource(dbName)
+
+beforeAll(() =>
+  setup(db)
+    .then(() => {
+      Object.assign(Repo, {
+        User: new UserRepository(db),
+        Degree: new DegreeRepository(db),
+        Graph: new GraphRepository(db),
+      })
+      return Promise.all([
+        Repo.User!.initialize(init.user1),
+        Repo.Degree!.initialize(init.degree1),
+      ])
+    })
+    .then(([user, degree]) => {
+      t.user = user
+      t.degree = degree
+      return Repo.User!.addDegree(t.user, t.degree.id)
+    })
+    .then(() =>
+      Repo.Graph!.initialize({
+        userId: t.user!.id,
+        degreeId: t.degree!.id,
+        modulesPlacedCodes: [],
+        modulesHiddenCodes: [],
+        pullAll: false,
+      })
+    )
+    .then((graph) => {
+      t.graph = graph
+    })
+)
+afterAll(() => teardown(db))
+
+it('user has 1 degree', async () => {
+  await Repo.User!.findOneById(t.user!.id).then((user) => {
+    expect(user.savedDegrees).toHaveLength(1)
+  })
+})
+
+it('successfully deletes', async () => {
+  await Repo.Degree!.remove(t.degree!).then((degree) => {
+    expect(degree.id).toEqual(undefined)
+  })
+})
+
+it('degree does not exist', async () => {
+  await expect(() => Repo.Degree!.findOneById(t.degree!.id)).rejects.toThrow(
+    Error
+  )
+})
+
+it('user exists', async () => {
+  await Repo.User!.findOneById(t.user!.id).then((user) => {
+    expect(user).toBeInstanceOf(User)
+  })
+})
+
+it('user has 0 degrees', async () => {
+  await Repo.User!.findOneById(t.user!.id).then((user) => {
+    expect(user.savedDegrees).toHaveLength(0)
+  })
+})
+
+it('graph does not exist', async () => {
+  await expect(() => Repo.Graph!.findOneById(t.graph!.id)).rejects.toThrow(
+    Error
+  )
+})
