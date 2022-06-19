56fb643cfd88bb9593d78a7e4d3a9177a592c1a0
weiseng18 <tws.hci@gmail.com>
2022-06-20 00:16:29 +0800

    feat(docs): add all other CS mods used in other degree API docs

--- apps/docs/data/degree/delete.ts
+++ apps/docs/data/degree/delete.ts
@@ -2,7 +2,27 @@ import { generateSchema } from 'utils'
 
 const fulfilled = {
   title: 'Computer Science',
-  modules: ['CS1101S', 'CS1231S', 'CS2030S', 'CS2040S'],
+  modules: [
+    'CS1101S',
+    'CS1231S',
+    'CS2030S',
+    'CS2040S',
+    'CS2100',
+    'CS2103T',
+    'CS2106',
+    'CS2109S',
+    'CS3230',
+    'CP3106',
+    'CP3209',
+    'CP4101',
+    'CP4106',
+    'CP3200',
+    'CP3202',
+    'CP3107',
+    'CP3110',
+    'IS4010',
+    'TR3202',
+  ],
 }
 
 export const deleteMethod: MethodProps = {
