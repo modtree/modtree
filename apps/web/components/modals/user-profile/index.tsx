e768f3d59b35913a2c23a0c5761305a534fa48ab
nguyenvukhang <brew4k@gmail.com>
2022-06-22 09:54:25 +0800

    feat: hide developer section in user profile sidebar

--- apps/web/components/modals/user-profile/index.tsx
+++ apps/web/components/modals/user-profile/index.tsx
@@ -6,9 +6,13 @@ import { contents } from './contents'
 
 export function UserProfileModal() {
   const showState = useAppSelector((state) => state.modal.showUserProfile)
+  const dev = process.env['NODE_ENV'] === 'development'
   return (
     <Modal showState={showState} hideAction={hideUserProfile} className="pr-0">
-      <SidebarWithContents show={showState} contents={contents} />
+      <SidebarWithContents
+        show={showState}
+        contents={contents.filter((e) => dev || e.category != 'Developer')}
+      />
     </Modal>
   )
 }
