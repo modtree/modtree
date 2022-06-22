922a40856f702d7e58bb452762875a976b84f457
nguyenvukhang <brew4k@gmail.com>
2022-06-22 11:03:35 +0800

    feat: h-full for panel layout in user-profile

--- apps/web/components/user-profile/layout/panels.tsx
+++ apps/web/components/user-profile/layout/panels.tsx
@@ -6,7 +6,7 @@ export default function Panels(props: { contents: ReactElement[] }) {
   return (
     <Tab.Panels className="flex-1 px-6 pb-12 overflow-y-auto">
       {props.contents.map((content, index) => (
-        <Tab.Panel key={dashed('user-profile-panel', index)}>
+        <Tab.Panel key={dashed('user-profile-panel', index)} className="h-full">
           {content}
         </Tab.Panel>
       ))}
