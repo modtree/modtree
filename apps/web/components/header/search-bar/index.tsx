0af228bccf6ef1e1e56f0d43150bbcd14a228d71
nguyenvukhang <brew4k@gmail.com>
2022-06-22 11:22:42 +0800

    feat: generalized search handler

--- apps/web/components/header/search-bar/index.tsx
+++ apps/web/components/header/search-bar/index.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import { Combobox } from '@headlessui/react'
 import { SearchIcon } from '@heroicons/react/solid'
 import { useAppDispatch } from '@/store/redux'
-import { handleSearch, getModuleInfo } from './lib'
+import { getModuleInfo } from './lib'
 import ResultEntries from './entries'
+import { handleSearch } from '@/utils/backend'
+import { clearSearches, setSearchedModuleCondensed } from '@/store/search'
 
 export default function SearchBar() {
   const dispatch = useAppDispatch()
@@ -21,7 +23,14 @@ export default function SearchBar() {
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-modtree-300">
             <Combobox.Input
               className="h-10 w-full border-none py-2 pl-3 pr-10 leading-5 text-gray-900 focus:ring-0"
-              onChange={(event) => handleSearch(dispatch, event.target.value)}
+              onChange={(event) =>
+                handleSearch({
+                  clear: clearSearches,
+                  set: setSearchedModuleCondensed,
+                  dispatch,
+                  value: event.target.value,
+                })
+              }
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
               <SearchIcon
--- apps/web/utils/backend.ts
+++ apps/web/utils/backend.ts
@@ -1,4 +1,7 @@
+import { ModuleCondensed } from '@modtree/entity'
+import { ActionCreatorWithOptionalPayload } from '@reduxjs/toolkit'
 import axios from 'axios'
+import { AnyAction, Dispatch } from 'redux'
 
 export const backend = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND,
@@ -6,4 +9,31 @@ export const backend = axios.create({
 
 export async function fetcher(url: string) {
   return axios.get(url).then((res) => res.data)
+}
+
+/**
+ * searches module condensed
+ */
+export async function handleSearch(props: {
+  dispatch: Dispatch
+  clear: () => AnyAction
+  set: ActionCreatorWithOptionalPayload<ModuleCondensed[], string>
+  value: string
+}) {
+  const { dispatch, value, clear, set } = props
+  if (value.length === 0) {
+    dispatch(clear())
+    return
+  }
+  const upper = value.toUpperCase()
+  const backend = process.env.NEXT_PUBLIC_BACKEND
+  const url = `${backend}/modules-condensed/${upper}/search`
+  fetch(url)
+    .then((res) => {
+      res.json().then((result) => {
+        const moduleList: ModuleCondensed[] = result
+        dispatch(set(moduleList.slice(0, 10)))
+      })
+    })
+    .catch(() => true)
 }
