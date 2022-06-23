cb512be31256b21479afb51fecd2a2b5e9fd534e
nguyenvukhang <brew4k@gmail.com>
2022-06-23 22:31:48 +0800

    test: add tests for find by key

--- libs/repo-base/src/index.ts
+++ libs/repo-base/src/index.ts
@@ -1,10 +1,4 @@
 export { useDeleteAll } from './delete-all'
 export { getDataSource } from './get-source'
 export { getRelationNames } from './get-relation-names'
-export { useFindOneByKey, useFindByKey } from './find-by-key'
-
-/**
- * standards:
- * - entity refers to an instance
- * - Entity refers to the class name
- */
+export { useFindOneByKey } from './find-by-key'
