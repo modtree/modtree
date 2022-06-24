a3c1350c9979dc94801aab1f9d39a7f67d968cbf
nguyenvukhang <brew4k@gmail.com>
2022-06-24 14:42:01 +0800

    fix: minimize string test

--- libs/repo-module/test/module/check-health/string.test.ts
+++ libs/repo-module/test/module/check-health/string.test.ts
@@ -3,20 +3,9 @@ import { flatten, oneUp } from '@modtree/utils'
 import { getSource } from '@modtree/typeorm-config'
 import { Module } from '@modtree/entity'
 import { validModuleCode } from '@modtree/utils'
-import { NUSMods } from '@modtree/types'
 
 const dbName = oneUp(__filename)
 const db = getSource(dbName)
-
-type PrereqTreeTestProps = {
-  string: string[]
-  json: NUSMods.PrereqTree[]
-}
-
-const tree: PrereqTreeTestProps = {
-  string: [],
-  json: [],
-}
 
 const notInDb = new Set<string>()
 let modules: Module[]
@@ -25,7 +14,7 @@ beforeAll(() =>
   setup(db)
     .then(() => Repo.Module!.find())
     .then((res) => {
-      modules = res
+      modules = res.filter((m) => typeof m.prereqTree === 'string')
     })
 )
 afterAll(() => teardown(db))
@@ -34,27 +23,21 @@ describe('module.prereqTree', () => {
   test('key exists', () => {
     modules.forEach((module) => {
       expect(module).toHaveProperty('prereqTree')
-      const prereqTree = module.prereqTree
-      if (typeof prereqTree === 'string') {
-        if (prereqTree !== '') tree.string.push(prereqTree)
-      } else {
-        tree.json.push(prereqTree)
-      }
     })
   })
 })
 
 describe('string prereqs', () => {
   test('are valid codes', () => {
-    tree.string.forEach((code) => {
-      expect(validModuleCode(code)).toBe(true)
+    modules.forEach((module) => {
+      expect(validModuleCode(module.moduleCode)).toBe(true)
     })
   })
   test('< 70 outside of database', () => {
     const dbCodes = modules.map(flatten.module)
-    tree.string.forEach((code) => {
-      if (!dbCodes.includes(code)) {
-        notInDb.add(code)
+    modules.forEach((module) => {
+      if (!dbCodes.includes(module.moduleCode)) {
+        notInDb.add(module.moduleCode)
       }
     })
     expect(notInDb.size).toBeLessThan(70)
