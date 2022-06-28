98ccb37d6c4f5abf1687c8726721b650e220a54a
nguyenvukhang <brew4k@gmail.com>
2022-06-28 11:54:38 +0800

    refactor

--- apps/web/store/cache.ts
+++ apps/web/store/cache.ts
@@ -1,6 +1,14 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { baseInitialState } from './initial-state'
 import { ModtreeApiResponse } from '@modtree/types'
+import { WritableDraft } from 'immer/dist/internal'
+
+const getState = <T>(state: WritableDraft<Record<string, T>>) => {
+  return {
+    existingCodes: new Set(Object.keys(state)),
+    newState: state,
+  }
+}
 
 export const cache = createSlice({
   name: 'cache',
@@ -10,8 +18,7 @@ export const cache = createSlice({
       cache,
       action: PayloadAction<ModtreeApiResponse.ModuleCondensed[]>
     ) => {
-      const existingCodes = new Set(Object.keys(cache.modulesCondensed))
-      const newState = cache.modulesCondensed
+      const { existingCodes, newState } = getState(cache.modulesCondensed)
       action.payload
         .filter((module) => !existingCodes.has(module.moduleCode))
         .forEach((module) => {
@@ -20,8 +27,7 @@ export const cache = createSlice({
       cache.modulesCondensed = newState
     },
     addModules: (cache, action: PayloadAction<ModtreeApiResponse.Module[]>) => {
-      const existingCodes = new Set(Object.keys(cache.modules))
-      const newState = cache.modules
+      const { existingCodes, newState } = getState(cache.modules)
       action.payload
         .filter((module) => !existingCodes.has(module.moduleCode))
         .forEach((module) => {
