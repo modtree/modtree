5b5556cced55a4c81ea2d99db4d1b3c1336ca1c7
nguyenvukhang <brew4k@gmail.com>
2022-06-28 11:05:32 +0800

    link modules modal to user modules

--- apps/web/components/user-profile/modules/main.tsx
+++ apps/web/components/user-profile/modules/main.tsx
@@ -1,41 +1,34 @@
-import { ModuleSimple, Pages } from 'types'
-import { ModuleStatus, SetState } from '@modtree/types'
+import { Pages } from 'types'
+import { SetState } from '@modtree/types'
 import { SettingsSection } from '@/ui/settings/lists/base'
 import { text } from 'text'
 import { dashed } from '@/utils/array'
 import { Row } from '@/ui/settings/lists/rows'
-
-type ModuleContent = Record<ModuleStatus, ModuleSimple[]>
-
-const moduleContent: ModuleContent = {
-  done: [
-    {
-      title: 'Linear Algebra',
-      moduleCode: 'MA2001',
-    },
-    {
-      title: 'Calculus',
-      moduleCode: 'MA2002',
-    },
-  ],
-  doing: [
-    {
-      title: 'Introduction to Geometry',
-      moduleCode: 'MA2219',
-    },
-    {
-      title: 'Design Thinking',
-      moduleCode: 'DTK1234',
-    },
-  ],
-  notTaken: [],
-}
+import { useAppSelector } from '@/store/redux'
+import { ModuleCondensed } from '@modtree/entity'
+import { useEffect } from 'react'
+import { backend } from '@/utils/backend'
 
 export function Main(props: { setPage: SetState<Pages['Modules']> }) {
+  const user = useAppSelector((state) => state.user)
   const hasModules = {
-    done: moduleContent.done.length !== 0,
-    doing: moduleContent.doing.length !== 0,
+    done: user.modulesDone.length !== 0,
+    doing: user.modulesDoing.length !== 0,
   }
+  const modules: Record<string, ModuleCondensed> = {}
+  useEffect(() => {
+    backend
+      .get<ModuleCondensed[]>('/modules-condensed', {
+        params: {
+          moduleCodes: ['CS1010', 'MA1100'],
+        },
+      })
+      .then((res) => {
+        res.data.forEach((module) => {
+          modules[module.moduleCode] = module
+        })
+      })
+  }, [])
   return (
     <>
       <div className="mb-12">
@@ -44,15 +37,15 @@ export function Main(props: { setPage: SetState<Pages['Modules']> }) {
           addButtonText="Add doing"
           onAddClick={() => props.setPage('add-doing')}
         >
-          {hasModules ? (
+          {hasModules.doing ? (
             <>
               <p>{text.moduleListSection.doing.summary}</p>
               <div className="ui-rectangle flex flex-col overflow-hidden">
-                {moduleContent.doing.map((module, index) => (
-                  <Row.Module key={dashed(module.moduleCode, index)}>
-                    <span className="font-semibold">{module.moduleCode}</span>
+                {user.modulesDoing.map((code, index) => (
+                  <Row.Module key={dashed(code, index)}>
+                    <span className="font-semibold">{code}</span>
                     <span className="mx-1">/</span>
-                    {module.title}
+                    {modules[code].title}
                   </Row.Module>
                 ))}
               </div>
@@ -68,15 +61,15 @@ export function Main(props: { setPage: SetState<Pages['Modules']> }) {
           addButtonText="Add done"
           onAddClick={() => props.setPage('add-done')}
         >
-          {hasModules ? (
+          {hasModules.done ? (
             <>
               <p>{text.moduleListSection.done.summary}</p>
               <div className="ui-rectangle flex flex-col overflow-hidden">
-                {moduleContent.done.map((module, index) => (
-                  <Row.Module key={dashed(module.moduleCode, index)}>
-                    <span className="font-semibold">{module.moduleCode}</span>
+                {user.modulesDone.map((code, index) => (
+                  <Row.Module key={dashed(code, index)}>
+                    <span className="font-semibold">{code}</span>
                     <span className="mx-1">/</span>
-                    {module.title}
+                    {modules[code].title}
                   </Row.Module>
                 ))}
               </div>
