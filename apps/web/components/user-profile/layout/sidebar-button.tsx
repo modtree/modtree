6854da61e87ed5e2ec3c4752db2069901761333d
nguyenvukhang <brew4k@gmail.com>
2022-06-28 13:18:37 +0800

    simplify sidebar button

--- apps/web/components/user-profile/layout/sidebar-button.tsx
+++ apps/web/components/user-profile/layout/sidebar-button.tsx
@@ -6,23 +6,20 @@ export default function SidebarButton(props: {
   children: string
   icon: HeroIcon
 }) {
-  const text = 'text-left text-base text-gray-600'
-  const shape = 'rounded-md'
-  const interact = 'hover:bg-gray-00 focus:outline-none'
-  const selectedCss = (s: boolean) =>
-    s ? ' font-medium text-gray-800 bg-gray-100' : ''
-  const iconSize = '16px'
   return (
     <Tab
       className={({ selected }) => {
-        return flatten('py-1 h-8', text, shape, interact, selectedCss(selected))
+        return flatten(
+          'py-1 h-8',
+          'text-left text-base text-gray-600',
+          'rounded-md',
+          'hover:bg-gray-00 focus:outline-none',
+          selected && 'font-medium text-gray-800 bg-gray-100'
+        )
       }}
     >
       <div className="flex flex-row items-center">
-        <props.icon
-          className="mx-2 text-gray-600"
-          style={{ height: iconSize, width: iconSize }}
-        />
+        <props.icon className="mx-2 text-gray-600" />
         {props.children}
       </div>
     </Tab>
