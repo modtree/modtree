4108e5208ed87a7b1d10285060fd1c40106e4eeb
nguyenvukhang <brew4k@gmail.com>
2022-06-28 22:21:03 +0800

    degree builder: fetch existing modules to editor

--- apps/web/components/user-profile/degrees/edit.tsx
+++ apps/web/components/user-profile/degrees/edit.tsx
@@ -5,9 +5,10 @@ import { Input } from '@/ui/html'
 import { Button } from '@/ui/buttons'
 import { ModtreeApiResponse, SetState } from '@modtree/types'
 import { SettingsSearchBox } from '@/ui/search'
-import { useAppSelector } from '@/store/redux'
+import { useAppDispatch, useAppSelector } from '@/store/redux'
 import { SelectedModules } from '../modules/selected-modules'
 import { backend } from '@/utils/backend'
+import { setBuildList } from '@/store/search'
 
 export function Edit(props: { setPage: SetState<Pages['Degrees']> }) {
   const { buildList, buildTitle, buildId } = useAppSelector(
@@ -16,13 +17,22 @@ export function Edit(props: { setPage: SetState<Pages['Degrees']> }) {
   const state = {
     title: useState<string>(buildTitle),
   }
+  const dispatch = useAppDispatch()
   useEffect(() => {
     const degree = backend
       .get<ModtreeApiResponse.Degree>(`/degree/${buildId}`)
       .then((res) => res.data)
-    degree.then((degree) => {
-      console.log('fetched degree', degree)
-    })
+    degree
+      .then((degree) => {
+        console.log('fetched degree', degree)
+        return degree.modules
+      })
+      .then((moduleCodes) =>
+        backend.get(`/modules-condensed`, { params: { moduleCodes } })
+      )
+      .then((res) => {
+        dispatch(setBuildList(res.data))
+      })
   }, [])
   return (
     <div className="flex flex-col">
