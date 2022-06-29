2a447ecbc14621e51a0a8f44b447bc722e268825
Khang <brew4k@gmail.com>
2022-06-29 16:20:50 +0800

    ci: read configs from `.github/scripts/ci.json` (#243)

---
+++ .github/scripts/read_json.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+[[ $EVENT == 'push' && $REF == 'refs/heads/main' ]] \
+  && is_push_to_main=true \
+  || is_push_to_main=false
+
+set_output() {
+  local JSON=`jq -rc "$1" .github/scripts/ci.json`
+  echo "::set-output name=$2::$JSON"
+}
+
+# install environment
+set_output '.install.command' 'install_command'
+
+# build environment
+set_output '.build.command' 'build_command'
+
+# test environment
+set_output '.test.command' 'test_command'
+set_output '.test.prefix | join(" ")' 'test_prefix'
+set_output '.test.projects' 'test_projects'
+
+# action-dependent environment
+echo "is push to main? $is_push_to_main"
+if [ "$is_push_to_main" = true ]; then
+  set_output '.build.args.no_cache | join(" ")' 'build_args'
+  set_output '.test.args.no_cache | join(" ")' 'test_args'
+else
+  set_output '.build.args.ci | join(" ")' 'build_args'
+  set_output '.test.args.ci | join(" ")' 'test_args'
+fi
