f11959d0ad433ed2db496ef42eb161da426fcefb
nguyenvukhang <brew4k@gmail.com>
2022-06-29 19:13:08 +0800

    feat: rehydrate doesnt depend on backend

--- apps/web/utils/rehydrate.ts
+++ apps/web/utils/rehydrate.ts
@@ -2,26 +2,24 @@ import { setDegree } from '@/store/degree'
 import { setGraph } from '@/store/graph'
 import store from '@/store/redux'
 import { setUser } from '@/store/user'
+import { api } from 'api'
 import { ModtreeUserContext } from 'types'
-import { backend } from './backend'
 
 const redux = store.getState()
 const dispatch = store.dispatch
 
 export function rehydrate(user: ModtreeUserContext['user']) {
   if (redux.user.id === '') {
-    backend
-      .get(`/user/${user.modtree.id}`)
-      .then((res) => dispatch(setUser(res.data)))
+    api.user.getById(user.modtree.id).then((user) => dispatch(setUser(user)))
   }
   if (redux.degree.id === '') {
-    backend.get(`/degree/${user.modtree.savedDegrees[0]}`).then((res) => {
-      dispatch(setDegree(res.data))
-    })
+    api.degree
+      .getById(user.modtree.savedDegrees[0])
+      .then((degree) => dispatch(setDegree(degree)))
   }
   if (redux.graph.id === '') {
-    backend
-      .get(`/graph/${user.modtree.savedGraphs[0]}`)
-      .then((res) => dispatch(setGraph(res.data)))
+    api.graph
+      .getById(user.modtree.savedGraphs[0])
+      .then((graph) => dispatch(setGraph(graph)))
   }
 }
