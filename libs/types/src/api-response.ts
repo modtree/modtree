b134c4b7fcffbb5b085df43aadab2eb432e97647
weiseng18 <tws.hci@gmail.com>
2022-06-29 19:06:55 +0800

    refactor: cleanup api-response types

--- libs/types/src/api-response.ts
+++ libs/types/src/api-response.ts
@@ -5,45 +5,10 @@ import {
   IModule,
   IModuleCondensed,
 } from './entity-interface'
-import { Modify } from './utils'
 
-/**
- * User API respsonse
- */
-export type User = Modify<
-  IUser,
-  {
-    modulesDone: IModule[]
-    modulesDoing: IModule[]
-    savedDegrees: IDegree[]
-    savedGraphs: IGraph[]
-    mainDegree: IDegree
-    mainGraph: IGraph
-  }
->
-
-/**
- * Degree API respsonse
- */
-export type Degree = Modify<
-  IDegree,
-  {
-    modules: IModule[]
-  }
->
-
-/**
- * Graph API respsonse
- */
-export type Graph = Modify<
-  IGraph,
-  {
-    user: IUser
-    degree: IDegree
-    modulesPlaced: IModule[]
-    modulesHidden: IModule[]
-  }
->
+export type User = IUser
+export type Degree = IDegree
+export type Graph = IGraph
 
 /**
  * already-flat entities
