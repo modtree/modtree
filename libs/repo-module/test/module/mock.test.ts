12cd062112b059391d7afea7995625af465a720a
nguyenvukhang <brew4k@gmail.com>
2022-07-01 17:58:16 +0800

    cleanup

--- libs/repo-module/test/module/mock.test.ts
+++ libs/repo-module/test/module/mock.test.ts
@@ -1,26 +1,10 @@
-import { DataSource } from 'typeorm'
-import { Module } from '@modtree/entity'
 import { ModuleRepository } from '@modtree/repo-module'
 import { empty } from '@modtree/utils'
 import '@modtree/test-env/jest'
+import { mocks } from '@modtree/test-env'
 
-function getFakeDb(): any {
-  const realDb = new DataSource({
-    database: 'yes',
-    type: 'postgres',
-    entities: [Module],
-  })
-  let db: any
-  db = realDb
-  db.getMetadata = () => ({
-    relations: [],
-  })
-  return db
-}
+const moduleRepo = new ModuleRepository(mocks.db)
 
-const db = getFakeDb()
-
-const moduleRepo = new ModuleRepository(db)
 moduleRepo.find = async () => [
   {
     ...empty.Module,
