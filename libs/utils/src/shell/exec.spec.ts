dd9e5e60279f41e1d5283a5cb098d09cbdeae130
nguyenvukhang <brew4k@gmail.com>
2022-07-01 23:46:36 +0800

    fix utils test

--- libs/utils/src/shell/exec.spec.ts
+++ libs/utils/src/shell/exec.spec.ts
@@ -2,13 +2,13 @@ import { exec } from '.'
 
 test('basic echo', async () => {
   await exec('echo "hello"').then((res) => {
-    expect(res.output).toBe('hello\n')
+    expect(res.stdout).toBe('hello\n')
   })
 })
 
 test('error handling', async () => {
   await exec('___invalid_command___').then((res) => {
-    const check = res.output.includes('___invalid_command___')
+    const check = res.stderr.includes('___invalid_command___')
     expect(check).toBe(true)
   })
 })
