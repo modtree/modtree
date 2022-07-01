fa36821d1f51760e46717e861589139c9fed20ae
weiseng18 <tws.hci@gmail.com>
2022-07-01 22:53:13 +0800

    allow jest to send request

--- apps/server/src/cors/index.ts
+++ apps/server/src/cors/index.ts
@@ -1,6 +1,10 @@
 import type { CorsOptions } from 'cors'
 
-const origins = ['https://modtree.vercel.app', 'http://localhost:3000']
+const origins = [
+  'https://modtree.vercel.app',
+  'http://localhost:3000',
+  'http://localhost',
+]
 
 export const corsOpts: CorsOptions = {
   origin: [...origins],
