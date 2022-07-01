aa8e26d9c8f766dc47b16110d2c6003bf7888e3d
weiseng18 <tws.hci@gmail.com>
2022-07-01 22:54:04 +0800

    update data

--- libs/repo-api/src/init.ts
+++ libs/repo-api/src/init.ts
@@ -150,9 +150,8 @@ export const auto: Init = {
 }
 
 type RouteInfo = {
-  pathParams?: Record<string, string>
-  queryParams?: Record<string, string>
-  bodyParams?: Record<string, string>
+  url: string
+  params?: Record<string, any>
 }
 
 type JestEach = ['post' | 'get' | 'patch' | 'delete', string, RouteInfo]
@@ -166,19 +165,24 @@ export const routes: JestEach[] = [
     'get',
     '/module-condensed/:moduleCode',
     {
-      pathParams: {
-        moduleCode: 'CS1010S',
+      url: '/module-condensed/CS1010S',
+    },
+  ],
+  [
+    'get',
+    '/modules-condensed',
+    {
+      url: '/modules-condensed',
+      params: {
+        moduleCodes: ['CS1010S', 'MA2001'],
       },
     },
   ],
-  ['get', '/modules-condensed', {}],
   [
     'get',
     '/search/modules-condensed/:searchQuery',
     {
-      pathParams: {
-        searchQuery: 'CS1010',
-      },
+      url: '/search/modules-condensed/CS1010',
     },
   ],
 ]
