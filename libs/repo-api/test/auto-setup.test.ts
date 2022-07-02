4441c4140a4869be0ad33d73b9477fd2b2ebc5ae
weiseng18 <tws.hci@gmail.com>
2022-07-02 17:06:19 +0800

    truncate response with huge arrays

--- libs/repo-api/test/auto-setup.test.ts
+++ libs/repo-api/test/auto-setup.test.ts
@@ -116,11 +116,23 @@ describe('samples', () => {
     // make request
     const res = await postman.send(method, url, { params })
 
+    // truncate response data if necessary
+    let response
+    if (Array.isArray(res.data) && res.data.length > 3) {
+      // if the response is an array, only give at most 3 entries
+      response = [res.data[0], res.data[1], res.data[2]]
+    } else {
+      // otherwise just leave it
+      response = res.data
+    }
+
+    // transform data
     const data = {
       method: res.config.method,
       url: res.config.url,
-      res: res.data,
+      res: response,
     }
+
     all.push(data)
 
     expect(1).toEqual(1)
