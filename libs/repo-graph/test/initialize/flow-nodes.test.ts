11df5c2b6fe02c0054f63214b355b468c5e47431
nguyenvukhang <brew4k@gmail.com>
2022-07-04 23:35:59 +0800

    fix: repo-graph test

--- libs/repo-graph/test/initialize/flow-nodes.test.ts
+++ libs/repo-graph/test/initialize/flow-nodes.test.ts
@@ -24,23 +24,34 @@ beforeAll(() =>
     .then(([user, degree]) => {
       t.user = user
       t.degree = degree
+      return Repo.Graph.initialize({
+        userId: t.user!.id,
+        degreeId: t.degree!.id,
+        modulesPlacedCodes: [],
+        modulesHiddenCodes: [],
+        pullAll: true,
+      })
+    })
+    .then((graph) => {
+      t.graph = graph
     })
 )
 afterAll(() => teardown(db))
 
-let nodes: GraphFlowNode[]
-let edges: GraphFlowEdge[]
+// for future deeper testing
+// let nodes: GraphFlowNode[]
+// let edges: GraphFlowEdge[]
 
 test('has flow nodes', () => {
   const graphNodes = t.graph!.flowNodes
   expect(graphNodes).toBeInstanceOf(Array)
   expect(graphNodes.length).toBeGreaterThan(0)
-  nodes = graphNodes
+  // nodes = graphNodes
 })
 
 test('has flow edges', () => {
   const graphEdges = t.graph!.flowEdges
   expect(graphEdges).toBeInstanceOf(Array)
   expect(graphEdges.length).toBeGreaterThan(0)
-  edges = graphEdges
+  // edges = graphEdges
 })
