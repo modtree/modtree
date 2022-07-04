9fe17ddfc5eca9d1a6ada99fd17a208d0bbe38e7
weiseng18 <tws.hci@gmail.com>
2022-07-04 16:23:44 +0800

    feat(postman): update user get

--- libs/postman/users/get.ts
+++ libs/postman/users/get.ts
@@ -1,13 +1,7 @@
 import axios from 'axios'
 
 axios
-  .get('http://localhost:8080/user', {
-    params: {
-      id: '58201858-5ce5-4ceb-8568-eecf55841b9f',
-      // authZeroId: 'auth0|62a8b56d430b9f20930583f7',
-      // email: 'brew4k@gmail.com',
-    },
-  })
+  .get('http://localhost:8080/user/7ffc9d39-8ac8-424e-a41a-6c017723cc7e')
   .then((res) => {
     console.debug(res.data)
   })
