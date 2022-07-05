d2ac4c7900f45e9cdd6375657ab2541a90c00020
nguyenvukhang <brew4k@gmail.com>
2022-07-05 16:16:38 +0800

    refactor: move out graph initializer again

--- libs/repo-graph/src/index.ts
+++ libs/repo-graph/src/index.ts
@@ -21,7 +21,8 @@ import { ModuleRepository } from '@modtree/repo-module'
 import { UserRepository } from '@modtree/repo-user'
 import { DegreeRepository } from '@modtree/repo-degree'
 import { getModules } from './get-modules'
-import { getFlowEdges, nodify } from './flow'
+import { getFlowEdges } from './get-edges'
+import { nodify } from './get-nodes'
 
 type ModuleState = 'placed' | 'hidden' | 'new'
 
