089a8994bf61f9b6445a299ffec43a69330dc6e9
weiseng18 <tws.hci@gmail.com>
2022-07-06 15:23:13 +0800

    util to convert saved graphs into DegreeGraph[]

---
+++ apps/web/utils/graph.ts
@@ -0,0 +1,35 @@
+import { Graph } from '@modtree/types'
+import { DegreeGraphs } from 'types'
+
+/**
+ * Converts a string to lowercase and
+ * replaces spaces with dashes.
+ *
+ * e.g. 'Computer Science' -> 'computer-science'
+ */
+export function lowercaseAndDash(str: string) {
+  return str.replace(/\s+/g, '-').toLowerCase()
+}
+
+export function getGraphContent(graphs: Graph[]): DegreeGraphs[] {
+  // convert into dictionary
+  // key: degreeId, value: array of graphIds
+  const dict: Record<string, string[]> = {}
+  graphs.forEach((graph) => {
+    const degreeTitle = lowercaseAndDash(graph.degree.title)
+    if (Object.keys(dict).includes(degreeTitle)) {
+      dict[degreeTitle].push(graph.id)
+    } else {
+      dict[degreeTitle] = [graph.id]
+    }
+  })
+  // flatten dictionary
+  const res: DegreeGraphs[] = []
+  Object.entries(dict).forEach(([key, value]) => {
+    res.push({
+      degree: key,
+      graphs: value,
+    })
+  })
+  return res
+}
