396ff4cca90e6b5ff7db834920a321dcca07179c
nguyenvukhang <brew4k@gmail.com>
2022-07-07 10:01:30 +0800

    shorten webpack

--- apps/server/webpack.js
+++ apps/server/webpack.js
@@ -2,11 +2,16 @@ const webpack = require('webpack')
 const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
+/**
+ * configuration paths
+ */
 const rootDir = path.resolve(__dirname, '../..')
-
 const outDir = path.resolve(rootDir, 'dist/apps/server')
-const entry = path.resolve(rootDir, 'apps/server/src/main.ts')
-const tsconfig = path.resolve(rootDir, 'apps/server/tsconfig.app.json')
+const entry = path.resolve(__dirname, 'src/main.ts')
+const tsconfig = path.resolve(__dirname, 'tsconfig.app.json')
+
+/** whether or not to watch for changes */
+const watch = false
 
 const compiler = webpack({
   entry,
@@ -15,15 +20,8 @@ const compiler = webpack({
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: tsconfig,
-            },
-          },
-        ],
+        test: /\.ts$/, // apply ts-loader on files ending with .ts
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
@@ -35,19 +33,17 @@ const compiler = webpack({
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-    plugins: [
-      new TsconfigPathsPlugin({
-        configFile: tsconfig,
-      }),
-    ],
+    /**
+     * this is what allows custom paths such as `@modtree/utils`
+     * to be resolved
+     */
+    plugins: [new TsconfigPathsPlugin({ configFile: tsconfig })],
   },
   output: {
     filename: 'bundle.js',
     path: outDir,
   },
 })
-
-const watch = true
 
 if (watch) {
   /**
