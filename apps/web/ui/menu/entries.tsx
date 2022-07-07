886e9109c2c886a51ccc6c49c350945f51e31735
nguyenvukhang <brew4k@gmail.com>
2022-07-07 14:39:58 +0800

    chore: cleanup entries.tsx

--- apps/web/ui/menu/entries.tsx
+++ apps/web/ui/menu/entries.tsx
@@ -5,6 +5,26 @@ import { flatten } from '@/utils/tailwind'
 import { dashed } from '@/utils/array'
 import { GraphFlowNode } from '@modtree/types'
 
+/**
+ * handles undefined, single, and array of children
+ */
+const Children = (props: { elements?: ReactElement | ReactElement[] }) => {
+  const { elements } = props
+  if (!elements) return null
+  if (Array.isArray(elements)) {
+    return (
+      <>
+        {elements.map((child, index) => (
+          <Menu.Item key={dashed('item-child', index)}>
+            <div className="px-4 py-3 text-gray-700">{child}</div>
+          </Menu.Item>
+        ))}
+      </>
+    )
+  }
+  return <Menu.Item>{elements}</Menu.Item>
+}
+
 export function Entries(props: {
   items: MenuItem[]
   children?: ReactElement | ReactElement[]
@@ -12,8 +32,6 @@ export function Entries(props: {
   roundedSelection?: boolean
   flowNode?: GraphFlowNode
 }) {
-  const { children, className, items } = props
-
   const onClick = (menuItem: MenuItem) => {
     /**
      * only run the callback if it exists
@@ -22,44 +40,26 @@ export function Entries(props: {
     menuItem.callback(props.flowNode)
   }
 
-  const Children = () => {
-    if (!children) return null
-    if (Array.isArray(children)) {
-      return (
-        <>
-          {children.map((child, index) => (
-            <Menu.Item key={dashed('item-child', index)}>
-              <div className="px-4 py-3 text-gray-700">{child}</div>
-            </Menu.Item>
-          ))}
-        </>
-      )
-    }
-    return <Menu.Item>{children}</Menu.Item>
-  }
-
   return (
     <>
-      <Children />
-      <div className={flatten('py-2', className)}>
-        {items.map((menuItem, index) => {
-          return (
-            <Menu.Item key={dashed(menuItem.text, index)}>
-              <a
-                href={menuItem.href}
-                onClick={() => onClick(menuItem)}
-                className={flatten(
-                  'hover:bg-modtree-400/80 hover:text-white',
-                  'text-gray-700 flex w-full px-4 py-1.5',
-                  'hover:no-underline',
-                  props.roundedSelection && 'rounded-md'
-                )}
-              >
-                {menuItem.text}
-              </a>
-            </Menu.Item>
-          )
-        })}
+      <Children elements={props.children} />
+      <div className={flatten('py-2', props.className)}>
+        {props.items.map((menuItem, index) => (
+          <Menu.Item key={dashed(menuItem.text, index)}>
+            <a
+              href={menuItem.href}
+              onClick={() => onClick(menuItem)}
+              className={flatten(
+                'hover:bg-modtree-400/80 hover:text-white',
+                'text-gray-700 flex w-full px-4 py-1.5',
+                'hover:no-underline',
+                props.roundedSelection && 'rounded-md'
+              )}
+            >
+              {menuItem.text}
+            </a>
+          </Menu.Item>
+        ))}
       </div>
     </>
   )
