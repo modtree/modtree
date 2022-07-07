86c4281763431cad7042fc40c60f58ad149401f1
nguyenvukhang <brew4k@gmail.com>
2022-07-07 10:36:53 +0800

    add a nodemon watcher

---
+++ apps/server/dev.js
@@ -0,0 +1,27 @@
+/**
+ * starts a process that constantly watches the entire workspace for updates
+ */
+
+require('./webpack')
+const nodemon = require('nodemon')
+const path = require('path')
+
+const rootDir = path.resolve(__dirname, '../..')
+
+nodemon({
+  script: path.resolve(rootDir, 'dist/apps/server/bundle.js'),
+  watch: path.resolve(rootDir, 'dist'),
+})
+
+nodemon
+  .on('start', function () {
+    console.log('App has started')
+  })
+  .on('quit', function () {
+    console.log('App has quit')
+    compiler.close()
+    process.exit()
+  })
+  .on('restart', function (files) {
+    console.log('App restarted due to: ', files)
+  })
