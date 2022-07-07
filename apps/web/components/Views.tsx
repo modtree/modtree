0bc0dae3b43a2d4dbbb3e25355df28bf8020c64e
nguyenvukhang <brew4k@gmail.com>
2022-07-07 14:01:54 +0800

    remove unused view

--- apps/web/components/Views.tsx
+++ apps/web/components/Views.tsx
@@ -19,15 +19,3 @@ export function FullScreenOverlay(props: { children: ReactChild }) {
     </div>
   )
 }
-
-/**
- * a header overlay that doesn't take any clicks on itself,
- * but still passes on click events to its children
- */
-export function HeaderOverlay(props: { children: ReactChild }) {
-  return (
-    <div className="z-10 fixed inset-0 h-16 w-screen py-2 pointer-events-none select-none">
-      {clickify(props.children)}
-    </div>
-  )
-}
