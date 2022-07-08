c3b7d0dbbe19c537a94994de2421e2ec656ef475
weiseng18 <tws.hci@gmail.com>
2022-07-08 15:24:46 +0800

    toggle to hidden now removes flowNodes and flowEdges

--- libs/repos/src/graph/repo.ts
+++ libs/repos/src/graph/repo.ts
@@ -194,8 +194,14 @@ export class GraphRepository
     }
     if (state === 'hidden') {
       toggle(graph.modulesHidden, graph.modulesPlaced)
+      // remove from flowNodes and flowEdges
+      graph.flowNodes = graph.flowNodes.filter((n) => n.id !== moduleCode)
+      graph.flowEdges = graph.flowEdges.filter(
+        (n) => n.source !== moduleCode && n.target !== moduleCode
+      )
       return this.save(graph)
     }
+
     return this.moduleRepo.findOneByOrFail({ moduleCode }).then((module) => {
       graph.modulesPlaced.push(module)
       return this.save(graph)
