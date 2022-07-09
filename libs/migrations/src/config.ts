e11a8055ba13b3a70d2ae2c44a654fc332ee8d41
nguyenvukhang <brew4k@gmail.com>
2022-07-09 19:05:45 +0800

    feat: add production check to migration

--- libs/migrations/src/config.ts
+++ libs/migrations/src/config.ts
@@ -14,6 +14,8 @@ const p = process.env
 const base = {
   entities: [ModuleCondensed, Module, User, Degree, Graph, ModuleFull],
   migrations,
+  synchronize: false,
+  migrationsRun: false,
 }
 
 const env = {
@@ -25,15 +27,32 @@ const env = {
     database: p.DEVELOPMENT_POSTGRES_DATABASE,
     ...base,
   },
+  production: {
+    port: parseInt(p.PRODUCTION_POSTGRES_PORT || '5432'),
+    host: p.PRODUCTION_POSTGRES_HOST,
+    username: p.PRODUCTION_POSTGRES_USERNAME,
+    password: p.PRODUCTION_POSTGRES_PASSWORD,
+    database: p.PRODUCTION_POSTGRES_DATABASE,
+    ...base,
+  },
 }
 
 const source = {
   development: new DataSource({
     type: 'postgres',
-    synchronize: false,
-    migrationsRun: false,
     ...env.development,
+  }),
+  production: new DataSource({
+    type: 'postgres',
+    ...env.production,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }),
 }
 
-export const migrationSource = source.development
+const useProd = false
+
+export const migrationSource = useProd ? source.production : source.development
