bc1517b39be08f6e8975078c795a9ab6064c76c0
weiseng18 <tws.hci@gmail.com>
2022-07-10 21:12:49 +0800

    add modules section to graph add-new

--- apps/web/components/user-profile/graphs/add-new.tsx
+++ apps/web/components/user-profile/graphs/add-new.tsx
@@ -25,11 +25,31 @@ export function AddNew(props: { setPage: SetState<Pages['Graphs']> }) {
       >
         <h6>Title</h6>
         <Input className="w-full mb-4" state={state.title} grayed />
+        <h6>Modules</h6>
+        <Modules />
         <h6>Degree</h6>
         <PickOne degrees={degrees} select={state.degree} pull={state.pullAll} />
       </SettingsSection>
       <div className="flex flex-row-reverse">
         <Button color="green">Save graph</Button>
+      </div>
+    </div>
+  )
+}
+
+function Modules() {
+  const modulesDone = useAppSelector((state) => state.user.modulesDone)
+  const modulesDoing = useAppSelector((state) => state.user.modulesDoing)
+  return (
+    <div className="mb-4">
+      <div className="bg-gray-200 p-2 rounded-xl flex-col space-y-2">
+        <p>The following modules will be placed in the graph:</p>
+        <p className="mb-0">
+          <b>Done:</b> {modulesDone.map((m) => m.moduleCode).join(', ')}
+        </p>
+        <p className="mb-0">
+          <b>Doing:</b> {modulesDoing.map((m) => m.moduleCode).join(', ')}
+        </p>
       </div>
     </div>
   )
