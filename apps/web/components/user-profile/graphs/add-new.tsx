1f84bc62cc98da434e558176b9c8b76b12e8c919
weiseng18 <tws.hci@gmail.com>
2022-07-10 21:16:57 +0800

    move modulesDone and modulesDoing to main component

--- apps/web/components/user-profile/graphs/add-new.tsx
+++ apps/web/components/user-profile/graphs/add-new.tsx
@@ -2,13 +2,16 @@ import { SettingsSection } from '@/ui/settings/lists/base'
 import { Button } from '@/ui/buttons'
 import { useState } from 'react'
 import { Input } from '@/ui/html'
-import { IDegree, SetState } from '@modtree/types'
+import { IDegree, IModule, SetState } from '@modtree/types'
 import { Pages } from 'types'
 import { PickOne } from '@/ui/search/degree/pick-one'
 import { useAppSelector } from '@/store/redux'
 
 export function AddNew(props: { setPage: SetState<Pages['Graphs']> }) {
   const degrees = useAppSelector((state) => state.user.savedDegrees)
+  const modulesDone = useAppSelector((state) => state.user.modulesDone)
+  const modulesDoing = useAppSelector((state) => state.user.modulesDoing)
+
   const state = {
     title: useState<string>(''),
     degree: useState<IDegree>(degrees[0]),
@@ -26,7 +29,7 @@ export function AddNew(props: { setPage: SetState<Pages['Graphs']> }) {
         <h6>Title</h6>
         <Input className="w-full mb-4" state={state.title} grayed />
         <h6>Modules</h6>
-        <Modules />
+        <Modules modulesDone={modulesDone} modulesDoing={modulesDoing} />
         <h6>Degree</h6>
         <PickOne degrees={degrees} select={state.degree} pull={state.pullAll} />
       </SettingsSection>
@@ -37,18 +40,16 @@ export function AddNew(props: { setPage: SetState<Pages['Graphs']> }) {
   )
 }
 
-function Modules() {
-  const modulesDone = useAppSelector((state) => state.user.modulesDone)
-  const modulesDoing = useAppSelector((state) => state.user.modulesDoing)
+function Modules(props: { modulesDone: IModule[]; modulesDoing: IModule[] }) {
   return (
     <div className="mb-4">
       <div className="bg-gray-200 p-2 rounded-xl flex-col space-y-2">
         <p>The following modules will be placed in the graph:</p>
         <p className="mb-0">
-          <b>Done:</b> {modulesDone.map((m) => m.moduleCode).join(', ')}
+          <b>Done:</b> {props.modulesDone.map((m) => m.moduleCode).join(', ')}
         </p>
         <p className="mb-0">
-          <b>Doing:</b> {modulesDoing.map((m) => m.moduleCode).join(', ')}
+          <b>Doing:</b> {props.modulesDoing.map((m) => m.moduleCode).join(', ')}
         </p>
       </div>
     </div>
