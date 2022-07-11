f351188283035283e5ef1a9f866aa5a646c9da98
nguyenvukhang <brew4k@gmail.com>
2022-07-11 12:05:36 +0800

    restore test

--- libs/repos/src/module/test/get-postreqs.test.ts
+++ libs/repos/src/module/test/get-postreqs.test.ts
@@ -3,38 +3,28 @@ import { mocks } from '@modtree/test-env'
 import '@modtree/test-env/jest'
 import { Module } from '@modtree/types'
 
-const database: Record<string, Partial<Module>> = {
+jest.mock('../../base')
+jest.mock('../../module')
+
+const fakeData: Record<string, Partial<Module>> = {
   ABC1000: { fulfillRequirements: ['shared1', 'shared2', 'ABC1000_only'] },
   XYZ1000: { fulfillRequirements: ['shared1', 'shared2', 'XYZ1000_only'] },
   DAB1000: { fulfillRequirements: [] },
 }
-
-jest.mock('../../base')
-jest.mock('../../module')
-// jest.mock('../repo', () => {
-//   const M = jest.requireActual('../repo')
-//   const R: typeof ModuleRepository = M.ModuleRepository
-//   // R.prototype.findByCodes = async (codes: string[]) => {
-//   //   return codes.map((code) => Object.assign(new Module(), database[code]))
-//   // }
-//   return { ModuleRepository: R }
-// })
-
-const moduleRepo = new ModuleRepository(mocks.db, database)
-
+const moduleRepo = new ModuleRepository(mocks.db, fakeData)
 const correct = [
-  // {
-  //   query: ['ABC1000'],
-  //   expected: ['shared1', 'shared2', 'ABC1000_only'],
-  // },
-  // {
-  //   query: ['XYZ1000'],
-  //   expected: ['shared1', 'shared2', 'XYZ1000_only'],
-  // },
-  // {
-  //   query: ['ABC1000', 'XYZ1000'],
-  //   expected: ['shared1', 'shared2', 'ABC1000_only', 'XYZ1000_only'],
-  // },
+  {
+    query: ['ABC1000'],
+    expected: ['shared1', 'shared2', 'ABC1000_only'],
+  },
+  {
+    query: ['XYZ1000'],
+    expected: ['shared1', 'shared2', 'XYZ1000_only'],
+  },
+  {
+    query: ['ABC1000', 'XYZ1000'],
+    expected: ['shared1', 'shared2', 'ABC1000_only', 'XYZ1000_only'],
+  },
   {
     query: ['DAB1000'],
     expected: [],
