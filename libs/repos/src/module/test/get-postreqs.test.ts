5e22728b7e95910f229fd53fa9fd773e79cba832
nguyenvukhang <brew4k@gmail.com>
2022-07-11 11:28:19 +0800

    mock get postreqs

--- libs/repos/src/module/test/get-postreqs.test.ts
+++ libs/repos/src/module/test/get-postreqs.test.ts
@@ -4,36 +4,9 @@ import '@modtree/test-env/jest'
 import { Module } from '@modtree/types'
 
 const database: Record<string, Partial<Module>> = {
-  CS1010: {
-    fulfillRequirements: [
-      'CP2106',
-      'CS1020',
-      'CS2030',
-      'CS2030S',
-      'CS2040',
-      'CS2040C',
-      'CS2040S',
-      'CS2100',
-      'CS2107',
-      'CS3237',
-      'DSA3102',
-      'MA3269',
-    ],
-  },
-  MA2001: {
-    fulfillRequirements: [
-      'MA2101',
-      'MA2101S',
-      'MA2213',
-      'MA2214',
-      'MA2219',
-      'MA2288',
-      'MA3220',
-      'MA3252',
-      'MA3238',
-      'MA2289',
-    ],
-  },
+  ABC1000: { fulfillRequirements: ['shared1', 'shared2', 'ABC1000_only'] },
+  XYZ1000: { fulfillRequirements: ['shared1', 'shared2', 'XYZ1000_only'] },
+  DAB1000: { fulfillRequirements: [] },
 }
 
 jest.mock('../../base')
@@ -48,46 +21,29 @@ jest.mock('../repo', () => {
 
 const moduleRepo = new ModuleRepository(mocks.db)
 
-it('returns an array', async () => {
-  await moduleRepo.getPostReqs(['MA2001']).then((res) => {
-    expect(res).toBeInstanceOf(Array)
-  })
-})
+const correct = [
+  {
+    query: ['ABC1000'],
+    expected: ['shared1', 'shared2', 'ABC1000_only'],
+  },
+  {
+    query: ['XYZ1000'],
+    expected: ['shared1', 'shared2', 'XYZ1000_only'],
+  },
+  {
+    query: ['ABC1000', 'XYZ1000'],
+    expected: ['shared1', 'shared2', 'ABC1000_only', 'XYZ1000_only'],
+  },
+  {
+    query: ['DAB1000'],
+    expected: [],
+  },
+]
 
-// describe('single query', () => {
-//
-//   it('returns correct modules', async () => {
-//     await Repo.Module.findOneByOrFail({
-//       moduleCode: 'MA2001',
-//     }).then((module) => {
-//       expect(t.postReqsCodes).toIncludeSameMembers(module.fulfillRequirements)
-//     })
-//   })
-// })
-//
-// describe('multi query', () => {
-//   it('returns an array', async () => {
-//     await Repo.Module.getPostReqs(['MA2001', 'CS1010S']).then((res) => {
-//       expect(res).toBeInstanceOf(Array)
-//       t.postReqsCodes = res
-//     })
-//   })
-//
-//   it('returns correct modules', async () => {
-//     const codes: string[] = []
-//     await Repo.Module.findBy({
-//       moduleCode: In(['MA2001', 'CS1010S']),
-//     }).then((modules) => {
-//       modules.forEach((module) => {
-//         codes.push(...module.fulfillRequirements)
-//       })
-//     })
-//     expect(t.postReqsCodes).toIncludeSameMembers(unique(codes))
-//   })
-// })
-//
-// it('returns [] for modules with no post reqs', async () => {
-//   await Repo.Module.getPostReqs(['CP2106']).then((res) => {
-//     expect(res).toStrictEqual([])
-//   })
-// })
+test.each(correct)(
+  'post-reqs of $query are correct',
+  async ({ query, expected }) => {
+    const res = await moduleRepo.getPostReqs(query)
+    expect(res).toIncludeSameMembers(expected)
+  }
+)
