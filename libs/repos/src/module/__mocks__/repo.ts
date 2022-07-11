22e9fd90e92efb3b96d113b6bea5488af3932a99
nguyenvukhang <brew4k@gmail.com>
2022-07-11 11:35:00 +0800

    write a fake db into module repo

--- libs/repos/src/module/__mocks__/repo.ts
+++ libs/repos/src/module/__mocks__/repo.ts
@@ -3,19 +3,29 @@ import { DataSource } from 'typeorm'
 import { ModuleRepository as Original } from '../repo'
 
 export class ModuleRepository extends Original {
-  constructor(db: DataSource) {
+  fakeDb: Record<string, Module>
+  constructor(db: DataSource, fakeDb?: Record<string, Partial<Module>>) {
     super(db)
+    Object.entries(fakeDb || {}).forEach(([key, value]) => {
+      this.fakeDb[key] = Object.assign(new Module(), value)
+    })
   }
 
   private moduleFromCode(moduleCode: string): Module {
     return Object.assign(new Module(), { moduleCode })
   }
 
+  private fetchOrFake(code: string): Module {
+    return Object.keys(this.fakeDb).includes(code)
+      ? this.fakeDb[code]
+      : this.moduleFromCode(code)
+  }
+
   override async findByCodes(moduleCodes: string[]): Promise<Module[]> {
-    return moduleCodes.map(this.moduleFromCode)
+    return moduleCodes.map(this.fetchOrFake)
   }
 
   override async findByCode(moduleCode: string): Promise<Module> {
-    return this.moduleFromCode(moduleCode)
+    return this.fetchOrFake(moduleCode)
   }
 }
