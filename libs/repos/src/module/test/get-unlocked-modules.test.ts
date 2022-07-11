8fdb988f3645f8e1944b24a36062966e383bd502
nguyenvukhang <brew4k@gmail.com>
2022-07-11 23:47:24 +0800

    fix: make module tests have valid codes

--- libs/repos/src/module/test/get-unlocked-modules.test.ts
+++ libs/repos/src/module/test/get-unlocked-modules.test.ts
@@ -8,15 +8,18 @@ jest.mock('../../module')
 const fakeData = {
   module: {
     // 1000 mods
-    A1000: { prereqTree: '', fulfillRequirements: ['A2000', 'C2000'] },
-    B1000: { prereqTree: '', fulfillRequirements: ['B2000'] },
-    C1000: { prereqTree: '', fulfillRequirements: ['C2000'] },
+    AX1000: { prereqTree: '', fulfillRequirements: ['AX2000', 'CX2000'] },
+    BX1000: { prereqTree: '', fulfillRequirements: ['BX2000'] },
+    CX1000: { prereqTree: '', fulfillRequirements: ['CX2000'] },
     // 2000 mods
-    C2000: { prereqTree: { or: ['A1000', 'C1000'] }, fulfillRequirements: [] },
-    A2000: { prereqTree: 'A1000', fulfillRequirements: ['A3000'] },
-    B2000: { prereqTree: 'B1000', fulfillRequirements: ['A3000'] },
+    CX2000: {
+      prereqTree: { or: ['AX1000', 'CX1000'] },
+      fulfillRequirements: [],
+    },
+    AX2000: { prereqTree: 'AX1000', fulfillRequirements: ['AX3000'] },
+    BX2000: { prereqTree: 'BX1000', fulfillRequirements: ['AX3000'] },
     // 3000 mods
-    A3000: { prereqTree: { or: ['A2000', 'B2000'] } },
+    AX3000: { prereqTree: { or: ['AX2000', 'BX2000'] } },
   },
 }
 
@@ -26,24 +29,24 @@ moduleRepo.getUnlockedModules
 const correct = [
   {
     type: 'unlocks one',
-    done: ['A1000'],
+    done: ['AX1000'],
     doing: [],
-    query: 'A2000',
-    expected: ['A3000'],
+    query: 'AX2000',
+    expected: ['AX3000'],
   },
   {
     type: "don't show if already done",
-    done: ['C1000', 'C2000'],
+    done: ['CX1000', 'CX2000'],
     doing: [],
-    query: 'A1000', // fulfills requirements for C2000
-    expected: ['A2000'], // but not C2000, since it's done
+    query: 'AX1000', // fulfills requirements for CX2000
+    expected: ['AX2000'], // but not CX2000, since it's done
   },
   {
     type: "don't show if already unlocked",
-    done: ['A1000', 'A2000'],
+    done: ['AX1000', 'AX2000'],
     doing: [],
-    query: 'B2000', // fulfills requirements for A2000
-    expected: [], // A3000 is already unlocked by A2000
+    query: 'BX2000', // fulfills requirements for AX2000
+    expected: [], // AX3000 is already unlocked by AX2000
   },
 ]
 
