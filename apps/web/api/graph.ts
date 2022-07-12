5b0969fca75f9d6cf6e1b8c2f3ee1a9993a3f8ad
weiseng18 <tws.hci@gmail.com>
2022-07-12 21:59:04 +0800

    add frontend helper

--- apps/web/api/graph.ts
+++ apps/web/api/graph.ts
@@ -48,4 +48,18 @@ export class GraphApi extends BaseApi {
   async create(props: InitGraphProps): Promise<ModtreeApiResponse.Graph> {
     return this.server.post(`/graph`, props).then((res) => res.data)
   }
+
+  /**
+   * can take modules
+   */
+  async canTakeModules(
+    graphId: string,
+    moduleCodes: string[]
+  ): Promise<Record<string, boolean>> {
+    return this.server
+      .post(`/graph/${graphId}/can-take-modules`, {
+        moduleCodes,
+      })
+      .then((res) => res.data)
+  }
 }
