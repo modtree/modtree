9ecceb99ecf1cb96db18962efdb5e8f39a1494d5
nguyenvukhang <brew4k@gmail.com>
2022-07-13 12:03:04 +0800

    use ncc to speed up test runner

--- scripts/run-tests/run.sh
+++ scripts/run-tests/run.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-echo $PWD
+# Reset
+_N_='\033[0m'     # Reset
+_R_='\033[0;31m'  # Red
+_G_='\033[0;32m'  # Green
+_Y_='\033[0;33m'  # Yellow
+_B_='\033[0;34m'  # Blue
+_P_='\033[0;35m'  # Purple
+_C_='\033[0;36m'  # Cyan
+_S_='\033[0;37m'  # Soft (Gray)
 
 run_node() {
-  echo "compiled test runner"
+  printf "\n🚀 compiled test runner\n"
   node dist/scripts/run-tests $@
 }
 
 run_tsnode() {
-  echo "ts-noded test runner"
+  printf "\n🍙 ts-noded test runner\n"
   yarn ts-node scripts/run-tests/run.ts $@
 }
 
