d06d15cf58e90fd7faf32e9813010463410a5429
nguyenvukhang <brew4k@gmail.com>
2022-07-13 14:26:02 +0800

    create a base repo 2

--- libs/repos/src/base/repo.ts
+++ libs/repos/src/base/repo.ts
@@ -9,3 +9,13 @@ export class BaseRepo<T> extends Repository<T> {
     this.relations = getRelations(this)
   }
 }
+
+export class BaseRepo2<T> extends Repository<T> {
+  relations: Relations
+  protected repo: Repository<T>
+  constructor(entity: EntityTarget<T>, db: DataSource) {
+    super(entity, db.manager)
+    this.repo = new Repository(entity, db.manager)
+    this.relations = getRelations(this)
+  }
+}
