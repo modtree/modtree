440744ac0da447b40f96d42f2dd55890b19cff7a
nguyenvukhang <brew4k@gmail.com>
2022-07-13 11:59:56 +0800

    install vercel ncc

--- scripts/run-tests/run.sh
+++ scripts/run-tests/run.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-yarn ts-node scripts/run-tests/run.ts $@
+echo $PWD
+
+run_node() {
+  echo "compiled test runner"
+  node dist/scripts/run-tests $@
+}
+
+run_tsnode() {
+  echo "ts-noded test runner"
+  yarn ts-node scripts/run-tests/run.ts $@
+}
+
+if [ -f dist/scripts/run-tests/index.js ]; then
+  run_node $@
+else
+  yarn test:build
+  [ -f dist/scripts/run-tests/index.js ] \
+    && run_node $@ \
+    || run_tsnode $@
+fi
 
 cleanup() {
   rm -f test.command
