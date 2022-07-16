301c0b294524fe4d667900b220833e6342cb4e90
weiseng18 <tws.hci@gmail.com>
2022-07-16 19:42:00 +0800

    use new route, use data-cy

--- apps/web-e2e/cypress/integration/search.cy.ts
+++ apps/web-e2e/cypress/integration/search.cy.ts
@@ -2,26 +2,19 @@ describe('search', () => {
   beforeEach(() => cy.visit('http://localhost:3000/'))
 
   it('should execute a search', () => {
-    /**
-     * intercept this particular API route and rename it to searchModules
-     */
-    cy.intercept('GET', '/search/modules/CS1010S').as('searchModules')
-    /**
-     * type in the search query
-     */
-    cy.get('[id^=headlessui-combobox-input-]').type('CS1010S')
-    /**
-     * wait for the last query (the full one) to resolve
-     */
-    cy.wait('@searchModules')
-    /**
-     * search for a module
-     */
-    cy.get('[id^=headlessui-combobox-option]')
-      .contains('Programming Methodology')
-      .click()
-    cy.get('h1').should('have.text', 'CS1010S')
-    cy.get('h2').should('have.text', 'Programming Methodology')
-    cy.get('#module-modal-close-button').click()
+    // intercept this particular API route and rename it to searchModules
+    cy.intercept('GET', '/trpc/search/modules').as('searchModules')
+
+    // type in the search query
+    cy.get('[data-cy=root-search-box]')
+      .type('CS1010S')
+      .then(() => {
+        cy.get('[data-cy=search-result]')
+          .contains('Programming Methodology')
+          .click()
+        cy.get('h1').should('have.text', 'CS1010S')
+        cy.get('h2').should('have.text', 'Programming Methodology')
+        cy.get('#module-modal-close-button').click()
+      })
   })
 })
