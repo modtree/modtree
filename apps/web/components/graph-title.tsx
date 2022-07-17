4837a2fbb38cba72b60231cc233f843b5ad929df
weiseng18 <tws.hci@gmail.com>
2022-07-17 16:35:38 +0800

    fix: first load

--- apps/web/components/graph-title.tsx
+++ apps/web/components/graph-title.tsx
@@ -1,4 +1,4 @@
-import { useAppSelector } from '@/store/redux'
+import store, { useAppSelector } from '@/store/redux'
 import { trpc } from '@/utils/trpc'
 import { ModtreeApiResponse } from '@modtree/types'
 import { useEffect, useState } from 'react'
@@ -13,9 +13,8 @@ export function FloatingGraphTitle() {
   const [graphs, setGraphs] = useState<ModtreeApiResponse.Graph[]>([])
   // placeholder state
   // updated correctly in useEffect
-  const mainGraph = useAppSelector((state) => state.graph)
   const state = {
-    graph: useState<ModtreeApiResponse.Graph>(mainGraph),
+    graph: useState<ModtreeApiResponse.Graph>(graphs[0]),
   }
 
   useEffect(() => {
@@ -24,6 +23,7 @@ export function FloatingGraphTitle() {
       // mainGraph is different from graphs
       // mainGraph has an extra property selectedCodes
       // removing that property is still not enough for React to check equality
+      const mainGraph = store.getState().graph
       const graph = graphs.find((g) => g.id === mainGraph.id)
       if (graph) state.graph[1](graph)
     })
@@ -35,7 +35,7 @@ export function FloatingGraphTitle() {
     'select-none bg-white shadow-md rounded-xl'
   )
 
-  return mainGraph.id !== '' ? (
+  return state.graph[0] && state.graph[0].id !== '' ? (
     <div className={className}>
       <GraphPicker graphs={graphs} select={state.graph} />
     </div>
