19e4a980f67113e6b99b41664a367b0d7b6580ea
nguyenvukhang <brew4k@gmail.com>
2022-07-17 21:48:02 +0800

    fix: trpc base url

--- apps/web/utils/trpc.ts
+++ apps/web/utils/trpc.ts
@@ -4,6 +4,8 @@ import type { AppRouter } from '@modtree/server'
 export const trpcReact = createReactQueryHooks<AppRouter>()
 // => { useQuery: ..., useMutation: ...}
 
+const envUrl = process.env.NEXT_PUBLIC_BACKEND
+
 export const trpc = createTRPCClient<AppRouter>({
-  url: 'http://localhost:8080/trpc',
+  url: envUrl ? envUrl + '/trpc' : 'http://localhost:8080/trpc',
 })
