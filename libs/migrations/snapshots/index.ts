4bd4b441b2a7db4e7d0d9c129df2f6e97aae29f1
nguyenvukhang <brew4k@gmail.com>
2022-07-19 09:41:41 +0800

    PRE: pre-migration

--- libs/migrations/snapshots/index.ts
+++ libs/migrations/snapshots/index.ts
@@ -1,2 +1,4 @@
 import { mod3To202207091657375809467 } from './1657375809467-mod3_to_20220709'
-export const migrations = [mod3To202207091657375809467]
+import { googleId1658194885583 } from './1658194885583-googleId'
+
+export const migrations = [mod3To202207091657375809467, googleId1658194885583]
