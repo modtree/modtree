f8228545763c5666f2ce55989cc0f2cab4d8fdc0
weiseng18 <tws.hci@gmail.com>
2022-07-20 14:20:54 +0800

    mocked login?

--- apps/web-e2e/cypress/support/commands.ts
+++ apps/web-e2e/cypress/support/commands.ts
@@ -16,6 +16,7 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Chainable {
       login(): void
+      login2(): void
       loadModuleModal<E>(
         moduleCode: string,
         title: string
@@ -33,6 +34,19 @@ declare global {
 
 Cypress.Commands.add('getCy', (value: string) => {
   return cy.get(`[data-cy=${value}]`)
+})
+
+Cypress.Commands.add('login2', () => {
+  cy.intercept('/api/auth/session', { fixture: 'session.json' }).as('session')
+
+  // Set the cookie for cypress.
+  // It has to be a valid cookie so next-auth can decrypt it and confirm its validity.
+  // This step can probably/hopefully be improved.
+  // We are currently unsure about this part.
+  // We need to refresh this cookie once in a while.
+  // We are unsure if this is true and if true, when it needs to be refreshed.
+  cy.setCookie('next-auth.session-token', Cypress.env('TOKEN'), { log: false })
+  Cypress.Cookies.preserveOnce('next-auth.session-token')
 })
 
 /**
