f1928fd76d0d40e06b3b911f7f2f0daf3f991e6c
nguyenvukhang <brew4k@gmail.com>
2022-07-21 22:13:04 +0800

    fix mc tests

--- libs/repos/src/module-condensed/test/initialize.test.ts
+++ libs/repos/src/module-condensed/test/initialize.test.ts
@@ -8,24 +8,19 @@ jest.mock('../../base')
 const moduleCondensedRepo = new ModuleCondensedRepository(mocks.db)
 let received: ModuleCondensed
 
-const init = {
-  id: '58201858-5ce5-4ceb-8568-eecf55841b9f',
-  title: 'Test Module',
-  moduleCode: 'TST1000MD',
-}
-
 it('returns a condensed module', async () => {
-  await moduleCondensedRepo.initialize(init).then((res) => {
-    expect(res).toBeInstanceOf(ModuleCondensed)
-    received = res
-  })
+  await moduleCondensedRepo
+    .initialize('Test Module', 'TST1000MD')
+    .then((res) => {
+      expect(res).toBeInstanceOf(ModuleCondensed)
+      received = res
+    })
 })
 
 test('saved all information', () => {
   const correct: [any, any][] = [
-    [received.id, init.id],
-    [received.title, init.title],
-    [received.moduleCode, init.moduleCode],
+    [received.title, 'Test Module'],
+    [received.moduleCode, 'TST1000MD'],
     [received.moduleLevel, 1000],
   ]
   correct.forEach(([received, expected]) => expect(received).toEqual(expected))
