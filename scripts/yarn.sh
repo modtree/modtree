ae9b28e281a5d133a4aff7da16853cb6aa0b956a
nguyenvukhang <brew4k@gmail.com>
2022-07-21 23:08:38 +0800

    update khang setup

--- scripts/yarn.sh
+++ scripts/yarn.sh
@@ -1,31 +1,41 @@
 #!/usr/bin/env bash
 
 # Reset
-_N_='\033[0m'     # Reset
-_R_='\033[0;31m'  # Red
-_G_='\033[0;32m'  # Green
-_Y_='\033[0;33m'  # Yellow
-_B_='\033[0;34m'  # Blue
-_P_='\033[0;35m'  # Purple
-_C_='\033[0;36m'  # Cyan
-_S_='\033[0;37m'  # Soft (Gray)
+_N_='\033[0m'    # Reset
+_R_='\033[0;31m' # Red
+_G_='\033[0;32m' # Green
+_Y_='\033[0;33m' # Yellow
+_B_='\033[0;34m' # Blue
+_P_='\033[0;35m' # Purple
+_C_='\033[0;36m' # Cyan
+_S_='\033[0;37m' # Soft (Gray)
 
-[[ $USER == "khang" ]]   && DOT_ENV=$HOME/dots/personal/.secrets/modtree/.env
+if [[ $USER == "khang" ]]; then
+  SRC=$HOME/dots/personal/.secrets/modtree
+  DOT_ENV=$SRC/.env
+  WEB_E2E=$SRC/cypress.env.json
+fi
 [[ $USER == "weiseng" ]] && DOT_ENV=$REPOS/orbital/env/.env
 
 env_start() {
- 	printf "\n${_S_}[installing .env files]${_N_}\n"
- 	printf "${_G_}.env:${_N_} $DOT_ENV\n"
+  printf "\n${_S_}[installing .env files]${_N_}\n"
+  printf "${_G_}.env:${_N_} $DOT_ENV\n"
 }
 
 env_install() {
-  ln -sf $DOT_ENV .
+  [ $DOT_ENV ] && ln -sf $DOT_ENV .
+  [ $WEB_E2E ] && ln -sf $WEB_E2E ./apps/web-e2e
 }
 
 env_end() {
-  [ -f ./.env ] \
-    && printf "\n${_G_} ✓ .env file loaded.${_N_}" \
-    || printf "\n${_Y_} ✗ .env file not loaded.${_N_}"
+  # root-level .env file
+  [ -f ./.env ] &&
+    printf "\n${_G_} ✓ .env file loaded.${_N_}" ||
+    printf "\n${_Y_} ✗ .env file not loaded.${_N_}"
+  # web-e2e test-runner credentials
+  [ -f ./apps/web-e2e/cypress.env.json ] &&
+    printf "\n${_G_} ✓ cypress.env.json loaded.${_N_}" ||
+    printf "\n${_Y_} ✗ cypress.env.json not loaded.${_N_}"
   printf "\n\n"
 }
 
