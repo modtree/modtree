cb35b175a8888977622f69861453f241cfd2535f
weiseng18 <tws.hci@gmail.com>
2022-07-24 15:13:48 +0800

    retire previous API docs

--- apps/docs/data/degree/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './get'
-export * from './list'
-export * from './delete'
-export * from './create'
--- apps/docs/data/graph/list.ts
+++
@@ -1,7 +0,0 @@
-export const list: MethodProps = {
-  method: 'List all graphs',
-  description: 'Retrieve information about all graphs',
-  endpoint: '/graphs/',
-  requestType: 'GET',
-  parameters: {},
-}
