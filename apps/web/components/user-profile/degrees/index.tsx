7b2d5abdb461e9586ac342b1c64b33491cfb2937
weiseng18 <tws.hci@gmail.com>
2022-07-24 23:31:02 +0800

    clear degree modal modules

--- apps/web/components/user-profile/degrees/index.tsx
+++ apps/web/components/user-profile/degrees/index.tsx
@@ -1,11 +1,19 @@
 import { Pages } from 'types'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Main } from './main'
 import { AddNew } from './add-new'
 import { Edit } from './edit'
+import { useAppDispatch, r } from '@/store/redux'
 
 export function DegreesTabContent() {
   const [page, setPage] = useState<Pages['Degrees']>('main')
+
+  /* clear degree modules on page change */
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(r.clearBuildList())
+  }, [page])
+
   if (page === 'main') {
     return <Main setPage={setPage} />
   }
