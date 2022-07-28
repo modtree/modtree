3036f11f56772988c15b7d5741a91500ec8ae4b8
weiseng18 <tws.hci@gmail.com>
2022-07-28 14:18:24 +0800

    feat: util to parse comma separated string

---
+++ apps/server/src/utils/parse.spec.ts
@@ -0,0 +1,33 @@
+import { parseCommaSeparatedString } from './parse'
+
+const correct = [
+  {
+    type: 'one moduleCode',
+    input: 'CS1010',
+    expected: ['CS1010'],
+  },
+  {
+    type: 'multiple moduleCodes',
+    input: 'CS1010, MA2001, EL1101E',
+    expected: ['CS1010', 'MA2001', 'EL1101E'],
+  },
+  {
+    type: 'one quoted UUID',
+    input: '009c5671-2e9c-4b15-850b-0469f7c8f62f',
+    expected: ['009c5671-2e9c-4b15-850b-0469f7c8f62f'],
+  },
+  {
+    type: 'one quoted string',
+    input: 'sample',
+    expected: ['sample'],
+  },
+  {
+    type: 'undefined input',
+    expected: [],
+  },
+]
+
+test.each(correct)('$type', (props) => {
+  const { input, expected } = props
+  expect(parseCommaSeparatedString(input)).toStrictEqual(expected)
+})
