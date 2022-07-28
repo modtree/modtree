bd80965fa25d37c00cc0f36e89e1509ae6758c1b
Khang <brew4k@gmail.com>
2022-07-28 11:40:35 +0800

    test(e2e): adding a new module to a new graph (#375)

--- apps/web-e2e/cypress/integration/graph/new-graph.cy.ts
+++ apps/web-e2e/cypress/integration/graph/new-graph.cy.ts
@@ -1,6 +1,7 @@
 function assertGraphCount(n: number) {
   /** wait for the first node to load */
-  cy.get('.react-flow__node')
+  cy.get('.react-flow__pane')
+  cy.wait('@getGraph')
   cy.window()
     .its('store')
     .invoke('getState')
@@ -11,21 +12,38 @@ function assertGraphCount(n: number) {
 }
 
 describe('add-and-remove', () => {
-  beforeEach(() => cy.login())
-
-  it('User has only one graph', () => {
-    assertGraphCount(1)
+  beforeEach(() => {
+    cy.intercept('GET', /.*graph.*/).as('getGraph')
+    cy.login()
   })
 
   it('Add a new graph', () => {
+    assertGraphCount(1)
     cy.addGraph('test-graph')
+  })
+
+  it('Add CS1231 to modules done', () => {
+    cy.openUserProfile('Modules')
+    cy.getCy('modify-doing').click()
+    cy.getCy('add-doing-search').clear().type('CS1231')
+    cy.getCy('search-result').contains('CS1231').click()
+    cy.getCy('build-list').should('contain.text', 'CS1231')
+    cy.get('button').contains('Save changes').click()
+    cy.closeUserProfile()
+  })
+
+  it('Add CS1231 to graph', () => {
+    cy.addGraphModule('CS1231', 'Discrete Structures')
+    // TODO: really start using node types for states so this isn't so clutch
+    cy.getCy('node-CS1231').should('have.css', 'color', 'rgb(75, 85, 99)')
+  })
+
+  it('Remove CS1231 from graph', () => {
+    cy.removeGraphModule('CS1231')
   })
 
   it('Remove created graph', () => {
     cy.removeGraph('test-graph')
-  })
-
-  it('User has one graph again', () => {
     assertGraphCount(1)
   })
 })
