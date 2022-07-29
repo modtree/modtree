626c453d82e478c076dd1d4e1018474663e5b95d
nguyenvukhang <brew4k@gmail.com>
2022-07-29 09:22:19 +0800

    test: run all cypress tests

--- test.js
+++
@@ -1,7 +0,0 @@
-const { spawnSync } = require('child_process')
-
-const sha = spawnSync('git', ['status', '--porcelain'], {
-  encoding: 'utf8',
-}).output[1]
-const count = (sha.match(/\n/) || []).length
-console.log(count)
