3db4abc5888017e9b608a0ae7bc8905170f55389
nguyenvukhang <brew4k@gmail.com>
2022-07-29 10:50:49 +0800

    refactor: move search and login

--- apps/web-e2e/cypress/integration/search.cy.ts
+++ apps/web-e2e/cypress/integration/search.cy.ts
@@ -1,15 +1,14 @@
 describe('search', () => {
-  beforeEach(() => cy.visit('/'))
-
-  it('should execute a search', () => {
+  before(() => cy.visit('/'))
+  it('works', () => {
     // type in the search query
     cy.getCy('root-search-box')
       .type('CS1010S')
-      .then(() => {
-        cy.getCy('search-result').contains('Programming Methodology').click()
-        cy.get('h1').should('have.text', 'CS1010S')
-        cy.get('h2').should('have.text', 'Programming Methodology')
-        cy.getCy('module-modal-close-button').click()
-      })
+      .getCy('search-result')
+      .contains('Programming Methodology')
+      .click()
+    cy.get('h1').should('have.text', 'CS1010S')
+    cy.get('h2').should('have.text', 'Programming Methodology')
+    cy.getCy('module-modal-close-button').click()
   })
 })
