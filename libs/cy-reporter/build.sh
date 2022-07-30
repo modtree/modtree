aa1a30d4af9dce412c8e89b17e2132e861d75581
nguyenvukhang <brew4k@gmail.com>
2022-07-30 18:46:25 +0800

    chore: remove unused files

--- libs/cy-reporter/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-OUT=dist/out-tsc/libs/cy-reporter/src/*.js
-DEST=apps/web-e2e/reporters
-
-tsc -p libs/cy-reporter/tsconfig.lib.json
-# mv $OUT $DEST
--- libs/cy-reporter/webpack.js
+++ libs/cy-reporter/webpack.js
@@ -3,80 +3,34 @@
  * into one executable pure nodejs file.
  */
 const webpack = require('webpack')
-const path = require('path')
+const { resolve } = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 /**
  * configuration paths
  */
-const rootDir = path.resolve(__dirname, '../..')
-const outDir = path.resolve(rootDir, 'dist/libs/cy-reporter')
-// const outDir = path.resolve(rootDir, 'apps/web-e2e/reporters')
-const d = (r) => path.resolve(__dirname, r)
-const entry = {
-  json: d('src/json.ts'),
-  list: d('src/list.ts'),
-  sender: d('src/sender.ts'),
-}
-const tsconfig = path.resolve(__dirname, 'tsconfig.lib.json')
-
-const defaultConfig = {
-  mode: 'development',
-  optimization: undefined,
-  build: false,
-}
+const rootDir = resolve(__dirname, '../..')
+const outDir = resolve(rootDir, 'dist/libs/cy-reporter')
+const tsconfig = resolve(__dirname, 'tsconfig.json')
 
 /**
- * handle arguments
+ * webpack config
  */
-const getArgsConfig = () => {
-  const config = defaultConfig
-  const args = process.argv.slice(2)
-  const handled = []
-  args.forEach((arg) => {
-    switch (arg) {
-      case '--build':
-        handled.push(arg)
-        config.build = true
-        break
-      case '--prod':
-        handled.push(arg)
-        config.mode = 'production'
-        config.optimization = { minimize: false }
-        break
-    }
-  })
-  console.log('args handled:', handled)
-  return config
-}
-const argsConfig = getArgsConfig()
-
 const compiler = webpack({
-  entry,
   target: 'node',
-  mode: argsConfig.mode,
-  optimization: argsConfig.optimization,
-  module: {
-    rules: [
-      {
-        test: /\.ts$/, // apply ts-loader on files ending with .ts
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+  mode: 'development',
+  entry: {
+    json: resolve(__dirname, 'src/json.ts'),
+    list: resolve(__dirname, 'src/list.ts'),
+    sender: resolve(__dirname, 'src/sender.ts'),
   },
-  plugins: [
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^pg-native$/,
-    }),
-  ],
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
+  },
+  plugins: [new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })],
   resolve: {
     roots: [rootDir],
     extensions: ['.tsx', '.ts', '.js'],
-    /**
-     * this is what allows custom paths such as `@modtree/utils`
-     * to be resolved
-     */
     plugins: [new TsconfigPathsPlugin({ configFile: tsconfig })],
   },
   output: {
@@ -86,14 +40,25 @@ const compiler = webpack({
   },
 })
 
-module.exports = compiler
+/**
+ * runs
+ */
+const args = process.argv.slice(2)
+const showErrors = (s) => (s.hasErrors() ? console.log(s.toString()) : null)
 
-if (argsConfig.build == true) {
-  /** run the build once */
-  compiler.run((_err, stats) => {
-    if (stats.hasErrors()) {
-      console.log(stats.toString())
-    }
-    compiler.close((_closeErr) => {})
+/**
+ * run the build once
+ */
+if (args.includes('--build')) {
+  compiler.run((_, stats) => showErrors(stats))
+}
+
+/**
+ * run the build and watch for changes
+ */
+if (args.includes('--watch')) {
+  compiler.watch({ aggregateTimeout: 300 }, (_, stats) => {
+    showErrors(stats)
+    console.log('built cy-reporter at', new Date().toLocaleString())
   })
 }
