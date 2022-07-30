767a1de93239449c1cce218133446f2f364e1845
weiseng18 <tws.hci@gmail.com>
2022-07-30 14:16:57 +0800

    only allow try it out on GET

--- apps/docs/components/pages/swagger.tsx
+++ apps/docs/components/pages/swagger.tsx
@@ -4,5 +4,16 @@ import 'swagger-ui-react/swagger-ui.css'
 const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false })
 
 export default function Swagger() {
-  return <SwaggerUI url="/openapi-docs.yml" />
+  /**
+   * only allow try it out on GET
+   * needs to be lowercase
+   */
+  const supportedMethods = ['get']
+
+  return (
+    <SwaggerUI
+      url="/openapi-docs.yml"
+      supportedSubmitMethods={supportedMethods}
+    />
+  )
 }
