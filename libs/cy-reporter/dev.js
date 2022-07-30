77b2f630bb4c35b178a663b38b7cf330a80a6f3a
nguyenvukhang <brew4k@gmail.com>
2022-07-30 11:17:49 +0800

    feat: minimize cy-reporter builder

--- libs/cy-reporter/dev.js
+++ libs/cy-reporter/dev.js
@@ -2,9 +2,7 @@
  * Starts a process that watches dist/apps/server for updates.
  */
 const compiler = require('./webpack')
-const nodemon = require('nodemon')
 const path = require('path')
-const rootDir = path.resolve(__dirname, '../..')
 
 /**
  * Starts a webpack daemon that recompiles the server bundle everytime there's
@@ -16,27 +14,3 @@ compiler.watch({ aggregateTimeout: 300 }, (_err, stats) => {
   }
   console.log('rebuild cy-reporter', new Date().toLocaleString())
 })
-
-/**
- * Calling nodemon immediately runs a listening node on the .script specified,
- * while watching the .watch directory for changes.
- */
-const outDir = 'apps/web-e2e/reporters'
-nodemon({
-  watch: path.resolve(rootDir, outDir),
-})
-
-/**
- * Breakdown for future debugging.
- */
-nodemon
-  .on('start', function () {
-    console.log('CyReporter has started')
-  })
-  .on('quit', function () {
-    console.log('CyReporter has quit')
-    process.exit()
-  })
-  .on('restart', function (files) {
-    console.log('CyReporter restarted due to: ', files)
-  })
