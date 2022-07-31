dab4ca408fd2271488ca7eb41ef4e8c44af29f49
weiseng18 <tws.hci@gmail.com>
2022-07-31 15:49:13 +0800

    get modules test

--- apps/server/test/utils/index.ts
+++ apps/server/test/utils/index.ts
@@ -2,8 +2,13 @@ import axios from 'axios'
 
 export class postman {
   static backend = 'http://localhost:8080'
+  static url = postman.backend + '/api'
 
   static async root() {
     return axios.get(postman.backend)
+  }
+
+  static async get(endpoint: string, params?: any) {
+    return axios.get(postman.url + endpoint, { params })
   }
 }
