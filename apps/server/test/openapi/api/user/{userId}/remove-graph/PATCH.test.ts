eb4746d682650a4453349e7a28e45928d21d514c
weiseng18 <tws.hci@gmail.com>
2022-08-01 20:05:09 +0800

    test: user graph methods

---
+++ apps/server/test/openapi/api/user/{userId}/remove-graph/PATCH.test.ts
@@ -0,0 +1,51 @@
+import { init, postman } from '../../../../../utils'
+
+let graphId: string
+let degreeId: string
+let userId: string
+
+it('create a graph', async () => {
+  const degree = postman.post('/degree', init.Degree)
+  const user = postman.post('/user', init.User)
+  return Promise.all([degree, user])
+    .then(([degreeRes, userRes]) => {
+      degreeId = degreeRes.data.data.id
+      userId = userRes.data.data.id
+      return [degreeId, userId]
+    })
+    .then(([degreeId, userId]) =>
+      postman.post('/graph', {
+        ...init.Graph,
+        userId,
+        degreeId,
+      })
+    )
+    .then((res) => {
+      expect(res.status).toBe(200)
+      graphId = res.data.data.id
+    })
+})
+
+it('insert a degree', async () => {
+  return postman
+    .patch(`/user/${userId}/insert-degrees`, { degreeIds: [degreeId] })
+    .then((res) => {
+      expect(res.status).toBe(200)
+    })
+})
+
+it('insert a graph', async () => {
+  return postman
+    .patch(`/user/${userId}/insert-graphs`, { graphIds: [graphId] })
+    .then((res) => {
+      expect(res.status).toBe(200)
+    })
+})
+
+it('remove a graph', async () => {
+  return postman
+    .patch(`/user/${userId}/remove-graph`, { graphId })
+    .then((res) => {
+      expect(res.status).toBe(200)
+    })
+})
