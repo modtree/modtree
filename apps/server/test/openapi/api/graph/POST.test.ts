9fdc197cd80f9ceb9d16e6541c39071d9800e842
weiseng18 <tws.hci@gmail.com>
2022-08-01 17:10:53 +0800

    test: all graph routes

---
+++ apps/server/test/openapi/api/graph/POST.test.ts
@@ -0,0 +1,27 @@
+import { init, postman } from '../../../utils'
+
+let degreeId: string
+let userId: string
+
+it('create a degree and user', async () => {
+  const degree = postman.post('/degree', init.Degree)
+  const user = postman.post('/user', init.User)
+  return Promise.all([degree, user]).then(([degreeRes, userRes]) => {
+    expect(degreeRes.status).toBe(200)
+    expect(userRes.status).toBe(200)
+    degreeId = degreeRes.data.data.id
+    userId = userRes.data.data.id
+  })
+})
+
+it('returns 200', async () => {
+  return postman
+    .post('/graph', {
+      ...init.Graph,
+      userId,
+      degreeId,
+    })
+    .then((res) => {
+      expect(res.status).toBe(200)
+    })
+})
