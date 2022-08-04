89428600ced09c6841974f277b142eab2f5e610b
weiseng18 <tws.hci@gmail.com>
2022-08-04 15:09:38 +0800

    move edges and nodes into its own folders

--- libs/utils/src/flow/redraw.ts
+++ libs/utils/src/flow/redraw.ts
@@ -1,6 +1,6 @@
 import { GraphFlowNode, GraphFrontendProps } from '@modtree/types'
-import { getFlowEdges } from './get-edges'
-import { getFlowNodes } from './get-nodes'
+import { getFlowEdges } from './edges'
+import { getFlowNodes } from './nodes'
 
 export const dagreify = (nodes: GraphFlowNode[]): GraphFrontendProps => {
   const flowEdges = getFlowEdges(nodes)
