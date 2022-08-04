ea7743eca181058983c78df7cec759780a63a501
nguyenvukhang <brew4k@gmail.com>
2022-08-04 22:09:38 +0800

    refactor: clean up build

--- docker/cy-reporter/build.js
+++ docker/cy-reporter/build.js
@@ -1,12 +1,14 @@
 require('dotenv/config')
 const path = require('path')
 const fs = require('fs')
-const { fork, spawn } = require('child_process')
+const { fork, spawnSync } = require('child_process')
 
 // paths
 const rootDir = path.resolve(__dirname, '../..')
 const tmpDir = path.resolve(__dirname, 'tmp')
 const root = (file) => path.resolve(rootDir, file)
+
+// heroku
 const image = 'modtree/cy-reporter'
 const herokuProject = 'modtree-cy-reporter'
 const herokuRegistry = 'registry.heroku.com'
@@ -17,6 +19,61 @@ const f = {
   dev: root('libs/cy-reporter/dev.js'),
   buildOutput: root('dist/libs/cy-reporter/server.js'),
   dockerfile: path.resolve(__dirname, 'Dockerfile'),
+}
+
+/**
+ * Publish the release on heroku.
+ * This requires the image to be already pushed to heroku's container registry.
+ */
+function herokuRelease() {
+  spawnSync('heroku', ['container:release', 'web', '--app', herokuProject], {
+    stdio: 'inherit',
+    env: { ...process.env, HEROKU_API_KEY: process.env['HEROKU_API_KEY'] },
+  })
+}
+
+/**
+ * Push the built image to the remote registry
+ */
+function pushImage() {
+  spawnSync('docker', ['push', herokuImage], { stdio: 'inherit' })
+}
+
+/**
+ * Authenticate with docker into heroku's container registry
+ */
+function dockerLogin() {
+  const pass = process.env['HEROKU_API_KEY']
+  spawnSync('docker', ['login', '-u', '_', '-p', pass, herokuRegistry], {
+    stdio: 'inherit',
+  })
+}
+
+function tagImage() {
+  spawnSync('docker', ['tag', `${image}:latest`, herokuImage], {
+    stdio: 'inherit',
+  })
+}
+
+// build the docker image of the server
+function buildImage() {
+  spawnSync('docker', ['build', '-t', image, '--file', f.dockerfile, tmpDir], {
+    stdio: 'inherit',
+  })
+}
+
+// run the webpack build
+function buildNode() {
+  const p = fork(f.dev, ['--build'], { stdio: 'inherit' })
+  p.on('close', (code) => {
+    if (code !== 0) throw new Error('build failed')
+    fs.copyFileSync(f.buildOutput, path.resolve(tmpDir, 'server.js'))
+    buildImage()
+    tagImage()
+    dockerLogin()
+    pushImage()
+    herokuRelease()
+  })
 }
 
 // exit cleanup catch
@@ -30,95 +87,5 @@ fs.rmSync(f.buildOutput, { force: true })
 // create a tmp directory for js build outputs
 fs.mkdirSync(tmpDir, { recursive: true })
 
-function herokuRelease() {
-  const p = spawn(
-    'heroku',
-    ['container:release', 'web', '--app', herokuProject],
-    {
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        HEROKU_API_KEY: process.env['HEROKU_API_KEY'],
-      },
-    }
-  )
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('heroku release failed')
-  })
-}
-
-function dockerLoginStdIn() {
-  const p = spawn(
-    'sh',
-    [
-      '-c',
-      `echo ${process.env['HEROKU_API_KEY']} | docker login ${herokuRegistry} -u _ --password-stdin`,
-    ],
-    { stdio: 'inherit' }
-  )
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('docker login failed')
-    pushImage()
-  })
-}
-
-function dockerLogin() {
-  const p = spawn(
-    'docker',
-    [
-      'login',
-      '--username=_',
-      `--password=${process.env['HEROKU_API_KEY']}`,
-      herokuRegistry,
-    ],
-    { stdio: 'inherit' }
-  )
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('docker login failed')
-    pushImage()
-  })
-}
-
-function pushImage() {
-  const p = spawn('docker', ['push', herokuImage], { stdio: 'inherit' })
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('push image failed')
-    herokuRelease()
-  })
-}
-
-function tagImage() {
-  const p = spawn('docker', ['tag', `${image}:latest`, herokuImage], {
-    stdio: 'inherit',
-  })
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('tag image failed')
-    dockerLogin()
-  })
-}
-
-// build the docker image of the server
-function dockerize() {
-  const p = spawn(
-    'docker',
-    ['build', '-t', image, '--file', f.dockerfile, tmpDir],
-    { stdio: 'inherit' }
-  )
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('dockerize failed')
-    tagImage()
-  })
-}
-
-// run the webpack build
-function build() {
-  const p = fork(f.dev, ['--build'], { stdio: 'inherit' })
-  p.on('close', (code) => {
-    if (code !== 0) throw new Error('build failed')
-    fs.copyFileSync(f.buildOutput, path.resolve(tmpDir, 'server.js'))
-    // herokuPush()
-    dockerize()
-  })
-}
-
-build()
+// start the build
+buildNode()
