95941930db82f4fd3dcf3ecf8fe94d0bbaa41378
nguyenvukhang <brew4k@gmail.com>
2022-08-04 14:18:53 +0800

    feat: list pretty-prints

--- libs/cy-reporter/src/reporters/list.ts
+++ libs/cy-reporter/src/reporters/list.ts
@@ -28,11 +28,10 @@ const params = {
   files: getAllFiles(specRoot).filter((f) => f.endsWith('.cy.ts')),
   commits: ancestryPath('origin/main', 'HEAD'),
 }
-client.get<Result[]>('/list', { params }).then((res) =>
+client.get<Result[]>('/list', { params }).then((res) => {
   res.data
     .sort((a, b) => order.indexOf(a.state) - order.indexOf(b.state))
     .forEach((r) => {
-      console.log('----', r.state)
       print[r.state](r.shortHash, r.file)
     })
-)
+})
