189d8c7dc9f3eac012d694df4afc399b9346d474
weiseng18 <tws.hci@gmail.com>
2022-08-04 18:50:23 +0800

    test: findByCode

---
+++ libs/repos/src/module/test/find-by-code.test.ts
@@ -0,0 +1,23 @@
+import { ModuleRepository } from '@modtree/repos'
+import { mocks } from '@modtree/test-env'
+import '@modtree/test-env/jest'
+
+jest.mock('../../base')
+jest.mock('../../module')
+
+const fakeData = {
+  module: [{ moduleCode: 'CS1010' }],
+}
+
+const moduleRepo = new ModuleRepository(mocks.getDb(fakeData))
+
+it('single code', async () => {
+  const received = await moduleRepo.findByCode('CS1010')
+  expect(received.moduleCode).toBe('CS1010')
+})
+
+it('invalid code', async () => {
+  expect(moduleRepo.findByCode('invalid_code')).rejects.toThrow(
+    Error('Invalid module code')
+  )
+})
