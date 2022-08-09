bfcd6b2a27ff482311da58a7076e00e02a5c97f9
nguyenvukhang <brew4k@gmail.com>
2022-08-09 20:48:59 +0800

    ci: update release server build

--- docker/server/build.js
+++ docker/server/build.js
@@ -1,10 +1,13 @@
 const { resolve } = require('path')
 const build = require('../build')
 const root = (f) => resolve(__dirname, '../..', f)
+const hasArg = (a) => process.argv.slice(2).includes(a)
+
+const herokuProject = hasArg('--prod') ? 'modtree' : 'modtree-dev'
 
 build({
   tmpDir: resolve(__dirname, 'tmp'),
-  herokuProject: 'modtree',
+  herokuProject,
   build: {
     module: root('apps/server/dev.js'),
     args: ['--build'],
