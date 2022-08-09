6a2dd7888549263268d273657b078c768317593b
nguyenvukhang <brew4k@gmail.com>
2022-08-09 23:55:07 +0800

    feat: generalize build-all

--- .github/scripts/build-all.js
+++ .github/scripts/build-all.js
@@ -4,13 +4,14 @@ const g = (...a) => console.log(gray(...a))
 
 /**
  * @param {string} name
+ * @param {string} cmd
  * @param {string[]} args
  * @returns {ChildProcessWithoutNullStreams}
  */
-function yarn(name, args) {
+function run(name, cmd, args) {
   const color = colors.pop()
   const fancy = '[' + color(name) + ']'
-  const p = spawn('yarn', args)
+  const p = spawn(cmd, args)
   p.stdout.setEncoding('utf8')
   p.stderr.setEncoding('utf8')
   p.stdout.on('data', (d) => g(fancy, d.trim()))
@@ -25,11 +26,11 @@ function yarn(name, args) {
 
 const colors = [blue, green, yellow]
 const builds = [
-  { name: 'web', args: ['build:web'] },
-  { name: 'server', args: ['build:server'] },
-  { name: 'docs', args: ['build:docs'] },
+  { name: 'web', cmd: 'yarn', args: ['build:web'] },
+  { name: 'server', cmd: 'yarn', args: ['build:server'] },
+  { name: 'docs', cmd: 'yarn', args: ['build:docs'] },
 ]
-const processes = builds.map((b) => yarn(b.name, b.args))
+const processes = builds.map((b) => run(b.name, b.cmd, b.args))
 
 function wipe() {
   processes.forEach((p) => {
