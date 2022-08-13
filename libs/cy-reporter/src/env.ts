2b471db41bd6369c5836a11f00c35588d7cffb7b
nguyenvukhang <brew4k@gmail.com>
2022-08-13 17:24:56 +0800

    no longer use dotenv in cy-reporter

--- libs/cy-reporter/src/env.ts
+++ libs/cy-reporter/src/env.ts
@@ -1,14 +1,3 @@
-/**
- * Some clutch assistance for dotenv to ensure that the env is read.
- * Meant to be imported as a file.
- */
-import { resolve } from 'path'
-import { config } from 'dotenv'
-
-// this is relative to the output directory,
-// namely apps/web-e2e/reporters
-config({ path: resolve(__dirname, '../../../.env') })
-
 // hard-coded because even in dev-time, we want to use the remote
 // server to read/write the runs
 export const useProd = false
